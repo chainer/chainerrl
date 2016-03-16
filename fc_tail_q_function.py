c17611dfe21f9fbb992c99961398462754f7ad04
muupan <muupan@gmail.com>
2016-03-16 19:22:54 +0900

    Remove unused imports

--- fc_tail_q_function.py
+++ fc_tail_q_function.py
@@ -1,11 +1,7 @@
-import numpy as np
-
 import chainer
-from chainer import functions as F
 from chainer import links as L
 
 import q_function
-import dqn_net
 
 
 class FCTailQFunction(chainer.ChainList, q_function.StateInputQFunction):
