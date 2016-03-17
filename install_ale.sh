9d0cb5a4d3094e274c192c03cb876dfdd7034edd
muupan <muupan@gmail.com>
2016-03-17 18:07:39 +0900

    Add a script to install ALE

---
+++ install_ale.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -e
+
+# Install ALE
+git clone https://github.com/mgbellemare/Arcade-Learning-Environment.git
+cd Arcade-Learning-Environment.git
+mkdir build
+cd build
+cmake -DUSE_SDL=ON -DUSE_RLGLUE=OFF -DBUILD_EXAMPLES=ON ..
+make -j 4
+cd ..
+pip install --user .
+
+# Get ROMs
+wget http://www.atarimania.com/roms/Roms.zip
+unzip Roms.zip
+unzip ROMS.zip
+ln -s ROMS/Video\ Olympics\ -\ Pong\ Sports\ \(Paddle\)\ \(1977\)\ \(Atari\,\ Joe\ Decuir\ -\ Sears\)\ \(CX2621\ -\ 99806\,\ 6-99806\,\ 49-75104\)\ ~.bin pong.bin
+ln -s ROMS/Breakout\ -\ Breakaway\ IV\ \(Paddle\)\ \(1978\)\ \(Atari\,\ Brad\ Stewart\ -\ Sears\)\ \(CX2622\ -\ 6-99813\,\ 49-75107\)\ ~.bin breakout.bin
