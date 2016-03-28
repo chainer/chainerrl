c941c0bfbb6cca9ca8d3c16adb96051fe30aaf6b
muupan <muupan@gmail.com>
2016-03-28 16:55:05 +0900

    Use logger and remove unused code

--- a3c.py
+++ a3c.py
@@ -1,4 +1,6 @@
 import copy
+from logging import getLogger
+logger = getLogger(__name__)
 
 import numpy as np
 import chainer
@@ -65,6 +67,7 @@ class A3C(agent.Agent):
                 R *= self.gamma
                 R += self.past_rewards[i]
                 v = self.v_function(self.past_states[i])
+                logger.debug('s:%s v:%s R:%s', self.past_states[i].sum(), v.data, R)
                 advantage = R - v
                 # Accumulate gradients of policy
                 log_prob = self.past_action_log_prob[i]
@@ -100,6 +103,7 @@ class A3C(agent.Agent):
             copy_param.copy_grad(self.shared_model, self.model)
             # Update the globally shared model
             self.optimizer.update()
+            logger.debug('update')
 
             self.sync_parameters()
 
@@ -118,7 +122,8 @@ class A3C(agent.Agent):
             self.past_action_entropy[self.t] = entropy
             self.t += 1
             if self.t % 100 == 0:
-                print 't:{} entropy:{} prob:{}'.format(self.t, entropy.data, np.exp(log_prob.data))
+                logger.debug('t:%s entropy:%s prob:%s',
+                             self.t, entropy.data, np.exp(log_prob.data))
             return action[0]
         else:
             return None
@@ -130,50 +135,3 @@ class A3C(agent.Agent):
     @property
     def optimizers(self):
         return [self.optimizer]
-
-
-def set_shared_params(a, b):
-    for param_name, param in a.namedparams():
-        if param_name in b:
-            shared_param = b[param_name]
-            param.data = np.frombuffer(shared_param.get_obj(
-            ), dtype=param.data.dtype).reshape(param.data.shape)
-
-
-def set_shared_states(a, b):
-    for state_name, shared_state in b.iteritems():
-        for param_name, param in shared_state.iteritems():
-            old_param = a._states[state_name][param_name]
-            a._states[state_name][param_name] = np.frombuffer(
-                param.get_obj(),
-                dtype=old_param.dtype).reshape(old_param.shape)
-
-
-def extract_params_as_shared_arrays(link):
-    shared_arrays = {}
-    for param_name, param in link.namedparams():
-        shared_arrays[param_name] = mp.Array('f', param.data.ravel())
-    return shared_arrays
-
-
-def extract_states_as_shared_arrays(optimizer):
-    shared_arrays = {}
-    for state_name, state in optimizer._states.iteritems():
-        shared_arrays[state_name] = {}
-        for param_name, param in state.iteritems():
-            shared_arrays[state_name][
-                param_name] = mp.Array('f', param.ravel())
-    return shared_arrays
-
-
-def create_a3c_agents(base_agent, num_process, envs):
-    raise NotImplementedError
-
-
-class A3CMaster(object):
-
-    def __init__(self):
-        raise NotImplementedError
-
-    def create_agent(self):
-        raise NotImplementedError
