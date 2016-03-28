826e08daa4d1a083accc73a4db7bf2933abb3997
muupan <muupan@gmail.com>
2016-03-28 15:49:32 +0900

    Make DQN heads more configurable

--- dqn_head.py
+++ dqn_head.py
@@ -5,15 +5,17 @@ from chainer import links as L
 
 class NatureDQNHead(chainer.ChainList):
 
-    def __init__(self, n_input_channels=4, activation=F.relu, bias=0.1):
+    def __init__(self, n_input_channels=4, n_output_channels=512,
+                 activation=F.relu, bias=0.1):
         self.n_input_channels = n_input_channels
         self.activation = activation
+        self.n_output_channels = n_output_channels
 
         layers = [
             L.Convolution2D(n_input_channels, 32, 8, stride=4, bias=bias),
             L.Convolution2D(32, 64, 4, stride=2, bias=bias),
             L.Convolution2D(64, 64, 3, stride=1, bias=bias),
-            L.Linear(3136, 512, bias=bias),
+            L.Linear(3136, n_output_channels, bias=bias),
         ]
 
         super(NatureDQNHead, self).__init__(*layers)
@@ -27,14 +29,16 @@ class NatureDQNHead(chainer.ChainList):
 
 class NIPSDQNHead(chainer.ChainList):
 
-    def __init__(self, n_input_channels=4, activation=F.relu):
+    def __init__(self, n_input_channels=4, n_output_channels=256,
+                 activation=F.relu, bias=0.1):
         self.n_input_channels = n_input_channels
         self.activation = activation
+        self.n_output_channels = n_output_channels
 
         layers = [
-            L.Convolution2D(n_input_channels, 16, 8, stride=4, bias=0.1),
-            L.Convolution2D(16, 32, 4, stride=2, bias=0.1),
-            L.Linear(2592, 256, bias=0.1),
+            L.Convolution2D(n_input_channels, 16, 8, stride=4, bias=bias),
+            L.Convolution2D(16, 32, 4, stride=2, bias=bias),
+            L.Linear(2592, n_output_channels, bias=bias),
         ]
 
         super(NIPSDQNHead, self).__init__(*layers)
