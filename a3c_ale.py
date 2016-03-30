9d2dbb41f64f5c17376f2533fa6abc108065c0a7
muupan <muupan@gmail.com>
2016-03-30 15:52:36 +0900

    Save models

--- a3c_ale.py
+++ a3c_ale.py
@@ -2,6 +2,10 @@ import multiprocessing as mp
 import os
 import argparse
 import random
+import tempfile
+import json
+import subprocess
+import sys
 
 import numpy as np
 
@@ -56,15 +60,45 @@ def main():
     parser.add_argument('processes', type=int)
     parser.add_argument('rom', type=str)
     parser.add_argument('--seed', type=int, default=None)
+    parser.add_argument('--outdir', type=str, default=None)
     parser.add_argument('--use-sdl', action='store_true')
     parser.add_argument('--t-max', type=int, default=5)
     parser.add_argument('--beta', type=float, default=1e-2)
     parser.add_argument('--profile', action='store_true')
+    parser.add_argument('--steps', type=int, default=10 ** 7)
     parser.set_defaults(use_sdl=False)
     args = parser.parse_args()
 
     if args.seed is not None:
         random_seed.set_random_seed(args.seed)
+
+    if args.outdir is not None:
+        if os.path.exists(args.outdir):
+            if not os.path.isdir(args.outdir):
+                raise RuntimeError('{} is not a directory'.format(args.outdir))
+        else:
+            os.makedirs(args.outdir)
+        outdir = args.outdir
+    else:
+        outdir = tempfile.mkdtemp()
+
+    print 'Output files are saved in {}'.format(outdir)
+
+    # Save all the arguments
+    with open(os.path.join(outdir, 'args.txt'), 'w') as f:
+        f.write(json.dumps(vars(args)))
+
+    # Save `git status`
+    with open(os.path.join(outdir, 'git-status.txt'), 'w') as f:
+        f.write(subprocess.check_output(['git', 'status']))
+
+    # Save `git log`
+    with open(os.path.join(outdir, 'git-log.txt'), 'w') as f:
+        f.write(subprocess.check_output(['git', 'log']))
+
+    # Save `git diff`
+    with open(os.path.join(outdir, 'git-diff.txt'), 'w') as f:
+        f.write(subprocess.check_output(['git', 'diff']))
 
     n_actions = ale.ALE(args.rom).number_of_actions
 
@@ -80,7 +114,7 @@ def main():
         #     lr=2.5e-4, alpha=0.95, momentum=0.95, eps=1e-2)
         model = chainer.ChainList(pi, v)
         opt.setup(model)
-        opt.add_hook(chainer.optimizer.GradientClipping(1))
+        opt.add_hook(chainer.optimizer.GradientClipping(2))
         return a3c.A3C(model, opt, args.t_max, 0.99, beta=args.beta, process_idx=process_idx)
 
     def env_func(process_idx):
@@ -89,24 +123,49 @@ def main():
     def run_func(process_idx, agent, env):
         total_r = 0
         episode_r = 0
+        max_score = None
 
-        for i in xrange(1000000):
+        try:
+            for i in xrange(args.steps):
 
-            total_r += env.reward
-            episode_r += env.reward
+                total_r += env.reward
+                episode_r += env.reward
 
-            action = agent.act(env.state, env.reward, env.is_terminal)
+                action = agent.act(env.state, env.reward, env.is_terminal)
 
-            if env.is_terminal:
-                if process_idx == 0:
-                    print 'i:{} episode_r:{}'.format(i, episode_r)
-                episode_r = 0
-                env.initialize()
-            else:
-                env.receive_action(action)
+                if env.is_terminal:
+                    if process_idx == 0:
+                        print 'i:{} episode_r:{}'.format(i, episode_r)
+                        if max_score == None or episode_r > max_score:
+                            if max_score is not None:
+                                # Save the best model so far
+                                print 'The best score is updated {} -> {}'.format(
+                                    max_score, episode_r)
+                                filename = os.path.join(
+                                    outdir, '{}.h5'.format(i))
+                                agent.save_model(filename)
+                                print 'Saved the current best model to {}'.format(
+                                    filename)
+                            max_score = episode_r
+                    episode_r = 0
+                    env.initialize()
+                else:
+                    env.receive_action(action)
+        except KeyboardInterrupt:
+            if process_idx == 0:
+                # Save the current model before being killed
+                agent.save_model(os.path.join(
+                    outdir, '{}_keyboardinterrupt.h5'.format(i)))
+                print >> sys.stderr, 'Saved the current model to {}'.format(
+                    outdir)
+            raise
 
         if process_idx == 0:
-            print 'pid:{}, total_r:{}'.format(os.getpid(), total_r)
+            print '{} pid:{}, total_r:{}'.format(outdir, os.getpid(), total_r)
+            # Save the final model
+            agent.save_model(
+                os.path.join(outdir, '{}_finish.h5'.format(args.steps)))
+            print 'Saved the current model to {}'.format(outdir)
 
     if args.profile:
 
