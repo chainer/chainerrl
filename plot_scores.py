9863492841d1f074a39f56c76813e26d46b9e804
muupan <muupan@gmail.com>
2016-05-05 00:18:19 +0900

    Specify path of scores.txt instead of dir

--- plot_scores.py
+++ plot_scores.py
@@ -1,16 +1,14 @@
 import argparse
-import os
 
-import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('dir', type=str)
+    parser.add_argument('scores', type=str, help='specify path of scores.txt')
     args = parser.parse_args()
-    scores_path = os.path.join(args.dir, 'scores.txt')
-    scores = pd.read_csv(scores_path, names=('t', 'score'), delimiter=' ')
+    scores = pd.read_csv(args.scores, names=('t', 'score'), delimiter=' ')
     scores.plot(x='t', y='score')
     plt.show()
 
