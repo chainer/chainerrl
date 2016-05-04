1da6a369bc660780ab3754b9a0abea082d5eab37
muupan <muupan@gmail.com>
2016-05-05 00:11:03 +0900

    Add a script to demo trained A3C

---
+++ demo_a3c_ale.py
@@ -0,0 +1,86 @@
+import argparse
+
+import numpy as np
+import chainer
+from chainer import serializers
+
+import policy
+import v_function
+import dqn_head
+import ale
+import random_seed
+
+
+def phi(screens):
+    assert len(screens) == 4
+    assert screens[0].dtype == np.uint8
+    raw_values = np.asarray(screens, dtype=np.float32)
+    # [0,255] -> [-128, 127]
+    raw_values -= 128
+    # [-128, 127] -> [-1, 1)
+    raw_values /= 128.0
+    return raw_values
+
+
+def eval_performance(rom, p_func, deterministic=False):
+    env = ale.ALE(rom, treat_life_lost_as_terminal=False)
+    test_r = 0
+    while not env.is_terminal:
+        s = chainer.Variable(np.expand_dims(phi(env.state), 0))
+        pout = p_func(s)
+        if deterministic:
+            a = pout.most_probable_actions[0]
+        else:
+            a = pout.action_indices[0]
+        test_r += env.receive_action(a)
+    return test_r
+
+
+def main():
+
+    import logging
+    logging.basicConfig(level=logging.DEBUG)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('rom', type=str)
+    parser.add_argument('model', type=str)
+    parser.add_argument('--seed', type=int, default=0)
+    parser.add_argument('--use-sdl', action='store_true')
+    parser.add_argument('--n-runs', type=int, default=10)
+    parser.add_argument('--deterministic', action='store_true')
+    parser.set_defaults(use_sdl=False)
+    parser.set_defaults(deterministic=False)
+    args = parser.parse_args()
+
+    random_seed.set_random_seed(args.seed)
+
+    n_actions = ale.ALE(args.rom).number_of_actions
+
+    def pv_func(model, state):
+        head, pi, v = model
+        out = head(state)
+        return pi(out), v(out)
+
+    # Load an A3C-DQN model
+    head = dqn_head.NIPSDQNHead()
+    pi = policy.FCSoftmaxPolicy(head.n_output_channels, n_actions)
+    v = v_function.FCVFunction(head.n_output_channels)
+    model = chainer.ChainList(head, pi, v)
+    serializers.load_hdf5(args.model, model)
+
+    def p_func(s):
+        head, pi, _ = model
+        out = head(s)
+        return pi(out)
+
+    scores = []
+    for i in range(args.n_runs):
+        score = eval_performance(
+            args.rom, p_func, deterministic=args.deterministic)
+        print('Run {}: {}'.format(i, score))
+        scores.append(score)
+    print('Average: {}'.format(sum(scores) / args.n_runs))
+
+
+if __name__ == '__main__':
+    main()
