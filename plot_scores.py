6f94fa1869893f6ee66476a0e3be80e6dda18511
muupan <muupan@gmail.com>
2016-05-21 15:31:11 +0900

    Receive --title argument

--- plot_scores.py
+++ plot_scores.py
@@ -7,20 +7,20 @@ import pandas as pd
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('scores', type=str, help='specify path of scores.txt')
+    parser.add_argument('--title', type=str, default=None)
     args = parser.parse_args()
 
     scores = pd.read_csv(args.scores, delimiter='\t')
     for col in ['mean', 'median']:
         plt.plot(scores['steps'], scores[col], label=col)
-    plt.title('A3C Breakout')
+    if args.title is not None:
+        plt.title(args.title)
     plt.xlabel('steps')
     plt.ylabel('score')
     plt.legend(loc='best')
-    # plt.show()
     fig_fname = args.scores + '.png'
     plt.savefig(fig_fname)
     print('Saved a figure as {}'.format(fig_fname))
 
 if __name__ == '__main__':
-
     main()
