ea98bacbaab8657ade67c7250154d3418d4bef57
muupan <muupan@gmail.com>
2016-05-21 14:42:17 +0900

    Add a script to synchronize async-rl repo

---
+++ sync-to-async-rl.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -u
+
+sync() {
+  cp $1 ../async-rl/$1
+}
+
+sync a3c_ale.py
+sync a3c.py
+sync ale.py
+sync async.py
+sync copy_param.py
+sync demo_a3c_ale.py
+sync dqn_head.py
+sync dqn_phi.py
+sync environment.py
+sync nonbias_weight_decay.py
+sync plot_scores.py
+sync policy_output.py
+sync policy.py
+sync prepare_output_dir.py
+sync random_seed.py
+sync rmsprop_async.py
+sync v_function.py
+sync init_like_torch.py
