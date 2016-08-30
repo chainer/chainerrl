570126c18672d5f45dac5ace97444e2ab35421c7
muupan <muupan@gmail.com>
2016-08-30 19:23:07 +0900

    Fix soft_copy_param for BatchNorm

--- copy_param.py
+++ copy_param.py
@@ -24,11 +24,16 @@ def soft_copy_param(target_link, source_link, tau):
     for param_name, param in source_link.namedparams():
         target_params[param_name].data[:] *= (1 - tau)
         target_params[param_name].data[:] += tau * param.data
-    if isinstance(target_link, L.BatchNormalization):
-        target_link.avg_mean[:] *= (1 - tau)
-        target_link.avg_mean[:] += tau * source_link.avg_mean
-        target_link.avg_var[:] *= (1 - tau)
-        target_link.avg_var[:] += tau * source_link.avg_var
+
+    # Soft-copy Batch Normalization's statistics
+    target_links = dict(target_link.namedlinks())
+    for link_name, link in source_link.namedlinks():
+        if isinstance(link, L.BatchNormalization):
+            target_bn = target_links[link_name]
+            target_bn.avg_mean[:] *= (1 - tau)
+            target_bn.avg_mean[:] += tau * link.avg_mean
+            target_bn.avg_var[:] *= (1 - tau)
+            target_bn.avg_var[:] += tau * link.avg_var
 
 
 def copy_grad(target_link, source_link):
