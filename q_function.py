07d3d63b095f36bf3a53b355586352b6df512bac
muupan <muupan@gmail.com>
2016-08-30 18:30:36 +0900

    Add FCBNSIContinuousQFunction

--- q_function.py
+++ q_function.py
@@ -90,6 +90,15 @@ class FCSIQFunction(chainer.ChainList, QFunction):
         return DiscreteQOutput(h)
 
 
+def scale_by_tanh(x, low, high):
+    xp = cuda.get_array_module(x.data)
+    scale = (high - low) / 2
+    scale = xp.expand_dims(xp.asarray(scale), axis=0)
+    mean = (high + low) / 2
+    mean = xp.expand_dims(xp.asarray(mean), axis=0)
+    return F.tanh(x) * scale + mean
+
+
 class FCSIContinuousQFunction(chainer.Chain, QFunction):
     """Fully-connected state-input continuous Q-function."""
 
@@ -141,11 +150,70 @@ class FCSIContinuousQFunction(chainer.Chain, QFunction):
         mu = self.mu(h)
 
         if self.scale_mu:
-            action_scale = (self.action_space.high - self.action_space.low) / 2
-            action_scale = xp.expand_dims(xp.asarray(action_scale), axis=0)
-            action_mean = (self.action_space.high + self.action_space.low) / 2
-            action_mean = xp.expand_dims(xp.asarray(action_mean), axis=0)
-            mu = F.tanh(mu) * action_scale + action_mean
+            mu = scale_by_tanh(mu, high=self.action_space.high,
+                               low=self.action_space.low)
+
+        mat_diag = F.exp(self.mat_diag(h))
+        if hasattr(self, 'mat_non_diag'):
+            mat_non_diag = self.mat_non_diag(h)
+            tril = lower_triangular_matrix(mat_diag, mat_non_diag)
+            mat = F.batch_matmul(tril, tril, transb=True)
+        else:
+            mat = F.expand_dims(mat_diag ** 2, axis=2)
+        return ContinuousQOutput(mu, mat, v, min_action=self.action_space.low,
+                                 max_action=self.action_space.high)
+
+
+class FCBNSIContinuousQFunction(chainer.Chain, QFunction):
+    """Fully-connected state-input continuous Q-function."""
+
+    def __init__(self, n_input_channels, n_dim_action, n_hidden_channels,
+                 n_hidden_layers, action_space, scale_mu=True,
+                 normalize_input=True):
+        """
+        Args:
+          n_input_channels: number of input channels
+          n_dim_action: number of dimensions of action space
+          n_hidden_channels: number of hidden channels
+          n_hidden_layers: number of hidden layers
+          action_space: action_space
+          scale_mu (bool): scale mu by applying tanh if True
+        """
+
+        self.n_input_channels = n_input_channels
+        self.n_hidden_layers = n_hidden_layers
+        self.n_hidden_channels = n_hidden_channels
+
+        assert action_space is not None
+        self.scale_mu = scale_mu
+        self.action_space = action_space
+
+        layers = {}
+
+        assert n_hidden_layers >= 1
+        layers['hidden_layers'] = MLPBN(
+              in_size=n_input_channels, out_size=n_hidden_channels,
+              hidden_sizes=[n_hidden_channels] * (n_hidden_layers - 1),
+              normalize_input=normalize_input)
+
+        layers['v'] = L.Linear(n_hidden_channels, 1)
+        layers['mu'] = L.Linear(n_hidden_channels, n_dim_action)
+        layers['mat_diag'] = L.Linear(n_hidden_channels, n_dim_action)
+        non_diag_size = n_dim_action * (n_dim_action - 1) // 2
+        if non_diag_size > 0:
+            layers['mat_non_diag'] = L.Linear(n_hidden_channels, non_diag_size)
+
+        super().__init__(**layers)
+
+    def __call__(self, state, test=False):
+        xp = cuda.get_array_module(state)
+        h = self.hidden_layers(state, test=test)
+        v = self.v(h)
+        mu = self.mu(h)
+
+        if self.scale_mu:
+            mu = scale_by_tanh(mu, high=self.action_space.high,
+                               low=self.action_space.low)
 
         mat_diag = F.exp(self.mat_diag(h))
         if hasattr(self, 'mat_non_diag'):
@@ -184,6 +252,7 @@ class FCSAQFunction(chainer.ChainList, QFunction):
                 L.Linear(self.n_hidden_channels, self.n_hidden_channels))
         layers.append(L.Linear(self.n_hidden_channels, 1))
         super().__init__(*layers)
+        self.output = layers[-1]
 
     def __call__(self, state, action, test=False):
         h = F.concat((state, action), axis=1)
