a33fc50b4b8da4e75d3572b00c576b030be7a329
muupan <muupan@gmail.com>
2016-09-10 17:19:09 +0900

    Replace raw_input with builtins.input

--- ask_yes_no.py
+++ ask_yes_no.py
@@ -1,6 +1,9 @@
+from builtins import input
+
+
 def ask_yes_no(question):
     while True:
-        choice = raw_input("{} [y/N]: ".format(question)).lower()
+        choice = input("{} [y/N]: ".format(question)).lower()
         if choice in ['y', 'ye', 'yes']:
             return True
         elif choice in ['n', 'no']:
