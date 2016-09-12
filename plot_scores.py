e2ea8d08d5e6006136b74d00a2ee64d1e7858941
muupan <muupan@gmail.com>
2016-09-12 14:38:54 +0900

    Support plotting multiple score files

--- plot_scores.py
+++ plot_scores.py
@@ -5,6 +5,7 @@ from __future__ import absolute_import
 from future import standard_library
 standard_library.install_aliases()
 import argparse
+import os
 
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -12,19 +13,32 @@ import pandas as pd
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('scores', type=str, help='specify path of scores.txt')
-    parser.add_argument('--title', type=str, default=None)
+    parser.add_argument('--title', type=str, default='')
+    parser.add_argument('--file', action='append', dest='files',
+                        default=[], type=str,
+                        help='specify paths of scores.txt')
+    parser.add_argument('--label', action='append', dest='labels',
+                        default=[], type=str,
+                        help='specify labels for scores.txt files')
     args = parser.parse_args()
 
-    scores = pd.read_csv(args.scores, delimiter='\t')
-    for col in ['mean', 'median']:
-        plt.plot(scores['steps'], scores[col], label=col)
-    if args.title is not None:
-        plt.title(args.title)
+    assert len(args.files) > 0
+    assert len(args.labels) == len(args.files)
+
+    for fpath, label in zip(args.files, args.labels):
+        if os.path.isdir(fpath):
+            fpath = os.path.join(fpath, 'scores.txt')
+        assert os.path.exists(fpath)
+        scores = pd.read_csv(fpath, delimiter='\t')
+        plt.plot(scores['steps'], scores['mean'], label=label)
+
     plt.xlabel('steps')
     plt.ylabel('score')
     plt.legend(loc='best')
-    fig_fname = args.scores + '.png'
+    if args.title:
+        plt.title(args.title)
+
+    fig_fname = args.files[0] + args.title + '.png'
     plt.savefig(fig_fname)
     print('Saved a figure as {}'.format(fig_fname))
 
