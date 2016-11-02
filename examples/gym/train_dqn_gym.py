5ea2b2ef171eaedebff0d56f5e3f6f1e057a571e
muupan <muupan@gmail.com>
2016-11-02 18:42:36 +0900

    Move files

--- examples/gym/train_dqn_gym.py
+++ examples/gym/train_dqn_gym.py
@@ -11,11 +11,12 @@ import sys
 from chainer import optimizers
 from chainer import cuda
 import gym
+gym.undo_logger_setup()
 import numpy as np
 from gym import spaces
 
-sys.path.append('..')
 from chainerrl.agents.dqn import DQN
+from chainerrl.agents.double_dqn import DoubleDQN
 from chainerrl.misc import random_seed
 from chainerrl import replay_buffer
 from chainerrl.experiments.prepare_output_dir import prepare_output_dir
@@ -24,6 +25,7 @@ from chainerrl import q_function
 from chainerrl.misc import env_modifiers
 from chainerrl.experiments.train_agent import train_agent_with_evaluation
 from chainerrl.explorers.epsilon_greedy import LinearDecayEpsilonGreedy
+from chainerrl.misc import reward_filter
 
 
 def main():
@@ -36,7 +38,7 @@ def main():
     parser.add_argument('--seed', type=int, default=None)
     parser.add_argument('--gpu', type=int, default=0)
     parser.add_argument('--final-exploration-steps',
-                        type=int, default=10 ** 6)
+                        type=int, default=10 ** 5)
     parser.add_argument('--start-epsilon', type=float, default=1.0)
     parser.add_argument('--end-epsilon', type=float, default=0.1)
     parser.add_argument('--model', type=str, default='')
@@ -45,13 +47,14 @@ def main():
     parser.add_argument('--target-update-frequency',
                         type=int, default=10 ** 4)
     parser.add_argument('--update-frequency', type=int, default=16)
-    parser.add_argument('--eval-n-runs', type=int, default=100)
-    parser.add_argument('--eval-frequency', type=int, default=10 ** 5)
+    parser.add_argument('--eval-n-runs', type=int, default=10)
+    parser.add_argument('--eval-frequency', type=int, default=10 ** 4)
     parser.add_argument('--n-x-layers', type=int, default=10)
     parser.add_argument('--n-hidden-channels', type=int, default=4)
     parser.add_argument('--gamma', type=float, default=0.99)
     parser.add_argument('--minibatch-size', type=int, default=32)
-    parser.add_argument('--render', action='store_true')
+    parser.add_argument('--render-train', action='store_true')
+    parser.add_argument('--render-eval', action='store_true')
     parser.add_argument('--reward-scale-factor', type=float, default=1e-3)
     parser.set_defaults(window_visible=False)
     args = parser.parse_args()
@@ -64,26 +67,23 @@ def main():
             a = cuda.to_cpu(a)
         return np.clip(a, action_space.low, action_space.high)
 
-    def reward_filter(r):
-        return r * args.reward_scale_factor
-
-    def make_env():
+    def make_env(for_eval):
         env = gym.make(args.env)
-        timestep_limit = env.spec.timestep_limit
-        env_modifiers.make_timestep_limited(env, timestep_limit)
+        env_modifiers.make_timestep_limited(env, env.spec.timestep_limit)
         if isinstance(env.action_space, spaces.Box):
             env_modifiers.make_action_filtered(env, clip_action_filter)
-        env_modifiers.make_reward_filtered(env, reward_filter)
-        if args.render:
+        if not for_eval:
+            env_modifiers.make_reward_filtered(
+                env, lambda x: x * args.reward_scale_factor)
+            # env_modifiers.make_reward_filtered(env, AverageRewardFilter())
+            # env_modifiers.make_reward_filtered(
+            #     env, reward_filter.NormalizedRewardFilter(scale=0.01))
+        if (args.render_eval and for_eval) or (args.render_train and not for_eval):
             env_modifiers.make_rendered(env)
-
-        def __exit__(self, *args):
-            pass
-        env.__exit__ = __exit__
         return env
 
-    env = make_env()
-    # timestep_limit = sample_env.spec.timestep_limit
+    env = make_env(for_eval=False)
+    timestep_limit = env.spec.timestep_limit
     obs_size = np.asarray(env.observation_space.shape).prod()
     action_space = env.action_space
 
@@ -102,7 +102,7 @@ def main():
     # Use the same hyper parameters as the Nature paper's
     # opt = optimizers.RMSpropGraves(
     #     lr=2.5e-4, alpha=0.95, momentum=0.95, eps=1e-2)
-    opt = optimizers.Adam()
+    opt = optimizers.Adam(eps=1e-2)
 
     opt.setup(q_func)
 
@@ -120,20 +120,23 @@ def main():
     explorer = LinearDecayEpsilonGreedy(
         args.start_epsilon, args.end_epsilon, args.final_exploration_steps,
         random_action)
-    agent = DQN(q_func, opt, rbuf, gpu=args.gpu, gamma=args.gamma,
-                explorer=explorer, replay_start_size=args.replay_start_size,
-                target_update_frequency=args.target_update_frequency,
-                update_frequency=args.update_frequency,
-                phi=phi, minibatch_size=args.minibatch_size)
+    agent = DoubleDQN(q_func, opt, rbuf, gpu=args.gpu, gamma=args.gamma,
+                      explorer=explorer, replay_start_size=args.replay_start_size,
+                      target_update_frequency=args.target_update_frequency,
+                      update_frequency=args.update_frequency,
+                      phi=phi, minibatch_size=args.minibatch_size)
     agent.logger.setLevel(logging.DEBUG)
 
     if len(args.model) > 0:
         agent.load_model(args.model)
 
+    eval_env = make_env(for_eval=True)
+
     train_agent_with_evaluation(
         agent=agent, env=env, steps=args.steps,
         eval_n_runs=args.eval_n_runs, eval_frequency=args.eval_frequency,
-        outdir=args.outdir)
+        outdir=args.outdir, eval_env=eval_env,
+        max_episode_len=timestep_limit)
 
 
 if __name__ == '__main__':
