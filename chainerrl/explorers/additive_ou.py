8dced4d5b7861e0a4d10aa98b352d455fa120224
muupan <muupan@gmail.com>
2016-12-28 16:35:50 +0900

    Add comments and clean interfaces of OU process

--- chainerrl/explorers/additive_ou.py
+++ chainerrl/explorers/additive_ou.py
@@ -3,7 +3,6 @@ from __future__ import unicode_literals
 from __future__ import print_function
 from __future__ import absolute_import
 from future import standard_library
-from future.utils import with_metaclass
 standard_library.install_aliases()
 
 from logging import getLogger
@@ -17,19 +16,22 @@ from chainerrl import explorer
 class AdditiveOU(explorer.Explorer):
     """Additive Ornstein-Uhlenbeck process.
 
-    Used in https://arxiv.org/abs/1509.02971."""
+    Used in https://arxiv.org/abs/1509.02971 for exploration.
 
-    def __init__(self, shape, mu=0.0, theta=0.15, sigma=0.3,
+    Args:
+        mu (float): Mean of the OU process
+        theta (float): Friction to pull towards the mean
+        sigma (float): Scale of noise
+    """
+
+    def __init__(self, mu=0.0, theta=0.15, sigma=0.3,
                  dt=1.0, logger=getLogger(__name__)):
-        self.shape = shape
         self.mu = mu
         self.theta = theta
         self.sigma = sigma
         self.dt = dt
         self.logger = logger
-
-        self.wiener_state = np.zeros(self.shape, dtype=np.float32)
-        self.ou_state = np.full(self.shape, self.mu, dtype=np.float32)
+        self.ou_state = None
 
     def evolve(self):
         # For a Wiener process, dW ~ N(0,u)
@@ -39,8 +41,10 @@ class AdditiveOU(explorer.Explorer):
             (self.mu - self.ou_state) * self.dt + self.sigma * dW
 
     def select_action(self, t, greedy_action_func):
-        self.evolve()
         a = greedy_action_func()
+        if self.ou_state is None:
+            self.ou_state = np.full(a.shape, self.mu, dtype=np.float32)
+        self.evolve()
         noise = self.ou_state
         self.logger.debug('t:%s noise:%s', t, noise)
         if isinstance(a, cuda.cupy.ndarray):
