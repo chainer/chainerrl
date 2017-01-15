f0ea457adb4e4bc1d8bc545d17199585eb78f844
muupan <muupan@gmail.com>
2017-01-15 17:48:03 +0900

    Reactivate DoublePAL and its tests

--- chainerrl/agents/double_pal.py
+++ chainerrl/agents/double_pal.py
@@ -6,59 +6,55 @@ from future import standard_library
 standard_library.install_aliases()
 import numpy as np
 
-from chainer import Variable
+import chainer
 from chainer import functions as F
 
 from chainerrl.agents import pal
+from chainerrl.recurrent import state_kept
 
 
 class DoublePAL(pal.PAL):
 
-    def _compute_y_and_t(self, experiences, gamma):
+    def _compute_y_and_t(self, exp_batch, gamma):
 
-        batch_size = len(experiences)
-
-        batch_state = self._batch_states(
-            [elem['state'] for elem in experiences])
+        batch_state = exp_batch['state']
+        batch_size = len(batch_state)
 
         qout = self.q_function(batch_state, test=False)
 
-        batch_actions = Variable(
-            self.xp.asarray([elem['action'] for elem in experiences]))
-        batch_q = F.reshape(qout.evaluate_actions(
-            batch_actions), (batch_size, 1))
+        batch_actions = exp_batch['action']
+        batch_q = qout.evaluate_actions(batch_actions)
 
         # Compute target values
 
-        target_qout = self.target_q_function(batch_state, test=True)
+        with chainer.no_backprop_mode():
+            target_qout = self.target_q_function(batch_state, test=True)
 
-        batch_next_state = self._batch_states(
-            [elem['next_state'] for elem in experiences])
+            batch_next_state = exp_batch['next_state']
 
-        next_qout = self.q_function(batch_next_state, test=False)
+            with state_kept(self.q_function):
+                next_qout = self.q_function(batch_next_state, test=False)
 
-        target_next_qout = self.target_q_function(batch_next_state, test=True)
-        next_q_max = target_next_qout.evaluate_actions(
-            next_qout.greedy_actions)
-        next_q_max.creator = None
+            with state_kept(self.target_q_function):
+                target_next_qout = self.target_q_function(
+                    batch_next_state, test=True)
+            next_q_max = F.reshape(target_next_qout.evaluate_actions(
+                next_qout.greedy_actions), (batch_size,))
 
-        batch_rewards = self.xp.asarray(
-            [elem['reward'] for elem in experiences], dtype=np.float32)
+            batch_rewards = exp_batch['reward']
+            batch_terminal = exp_batch['is_state_terminal']
 
-        batch_terminal = self.xp.asarray(
-            [elem['is_state_terminal'] for elem in experiences],
-            dtype=np.float32)
+            # T Q: Bellman operator
+            t_q = batch_rewards + self.gamma * \
+                (1.0 - batch_terminal) * next_q_max
 
-        # T Q: Bellman operator
-        t_q = batch_rewards + self.gamma * (1.0 - batch_terminal) * next_q_max
-
-        # T_PAL Q: persistent advantage learning operator
-        cur_advantage = target_qout.compute_advantage(batch_actions)
-        next_advantage = target_next_qout.compute_advantage(batch_actions)
-        tpal_q = t_q + self.alpha * F.maximum(cur_advantage, next_advantage)
-
-        batch_q = F.reshape(batch_q, (batch_size, 1))
-        tpal_q = F.reshape(tpal_q, (batch_size, 1))
-        tpal_q.creator = None
+            # T_PAL Q: persistent advantage learning operator
+            cur_advantage = F.reshape(
+                target_qout.compute_advantage(batch_actions), (batch_size,))
+            next_advantage = F.reshape(
+                target_next_qout.compute_advantage(batch_actions),
+                (batch_size,))
+            tpal_q = t_q + self.alpha * \
+                F.maximum(cur_advantage, next_advantage)
 
         return batch_q, tpal_q
--- tests/agents_tests/test_double_pal.py
+++ tests/agents_tests/test_double_pal.py
@@ -6,14 +6,31 @@ from future import standard_library
 standard_library.install_aliases()
 
 from chainerrl.agents.double_pal import DoublePAL
-from chainerrl import replay_buffer
-from test_dqn_like import _TestDQNLike
+from test_dqn_like import _TestDQNOnDiscreteABC
+from test_dqn_like import _TestDQNOnDiscretePOABC
+from test_dqn_like import _TestDQNOnContinuousABC
 
 
-class TestDoublePAL(_TestDQNLike):
+class TestDoublePALOnDiscreteABC(_TestDQNOnDiscreteABC):
 
-    def make_agent(self, gpu, q_func, explorer, opt):
-        rbuf = replay_buffer.ReplayBuffer(10 ** 5)
-        return DoublePAL(q_func, opt, rbuf, gpu=gpu, gamma=0.9,
-                         explorer=explorer, replay_start_size=100,
-                         target_update_frequency=100)
+    def make_dqn_agent(self, env, q_func, opt, explorer, rbuf, gpu):
+        return DoublePAL(
+            q_func, opt, rbuf, gpu=gpu, gamma=0.9, explorer=explorer,
+            replay_start_size=100, target_update_frequency=100)
+
+
+class TestDoublePALOnContinuousABC(_TestDQNOnContinuousABC):
+
+    def make_dqn_agent(self, env, q_func, opt, explorer, rbuf, gpu):
+        return DoublePAL(
+            q_func, opt, rbuf, gpu=gpu, gamma=0.9, explorer=explorer,
+            replay_start_size=100, target_update_frequency=100)
+
+
+class TestDoublePALOnDiscretePOABC(_TestDQNOnDiscretePOABC):
+
+    def make_dqn_agent(self, env, q_func, opt, explorer, rbuf, gpu):
+        return DoublePAL(
+            q_func, opt, rbuf, gpu=gpu, gamma=0.9, explorer=explorer,
+            replay_start_size=100, target_update_frequency=100,
+            episodic_update=True)
