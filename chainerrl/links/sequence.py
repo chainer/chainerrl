fd11b94bca25d3f48624142cc3ee63879ec5f191
muupan <muupan@gmail.com>
2017-01-23 23:43:01 +0900

    Make Sequence.__call__ faster

--- chainerrl/links/sequence.py
+++ chainerrl/links/sequence.py
@@ -17,13 +17,15 @@ class Sequence(chainer.ChainList, RecurrentChainMixin):
     def __init__(self, *layers):
         self.layers = layers
         links = [layer for layer in layers if isinstance(layer, chainer.Link)]
+        # Cache the results because getargspec is slow
+        self.argnames = [set(inspect.getargspec(layer)[0])
+                         for layer in layers]
         super().__init__(*links)
 
     def __call__(self, x, **kwargs):
         h = x
-        for layer in self.layers:
-            layer_argnames = inspect.getargspec(layer)[0]
+        for layer, argnames in zip(self.layers, self.argnames):
             layer_kwargs = {k: v for k, v in kwargs.items()
-                            if k in layer_argnames}
+                            if k in argnames}
             h = layer(h, **layer_kwargs)
         return h
