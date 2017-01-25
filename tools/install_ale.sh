b63c90b488d618310c012126254ad84f0d1dde54
muupan <muupan@gmail.com>
2017-01-25 16:04:47 +0900

    Stop downloading roms since we have atari_py

--- tools/install_ale.sh
+++ tools/install_ale.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -eu
+set -Ceu
 
 if ! hash git 2>/dev/null; then
   echo "You need git"
@@ -12,11 +12,6 @@ if ! hash cmake 2>/dev/null; then
   exit 1
 fi
 
-if ! hash unzip 2>/dev/null; then
-  echo "You need unzip"
-  exit 1
-fi
-
 # Install ALE
 if [ ! -e Arcade-Learning-Environment ]; then
   git clone https://github.com/mgbellemare/Arcade-Learning-Environment.git
@@ -25,24 +20,3 @@ cd Arcade-Learning-Environment
 cmake -DUSE_SDL=ON -DUSE_RLGLUE=OFF -DBUILD_EXAMPLES=ON .
 make -j 4
 pip install --user .
-
-# Get ROMs
-cd ..
-if [ ! -e Roms.zip ]; then
-  wget http://www.atarimania.com/roms/Roms.zip
-fi
-if [ ! -e ROMS.zip ]; then
-  unzip Roms.zip
-fi
-if [ ! -e ROMS ]; then
-  unzip ROMS.zip
-fi
-if [ ! -e pong.bin ]; then
-  ln -s ROMS/Video\ Olympics\ -\ Pong\ Sports\ \(Paddle\)\ \(1977\)\ \(Atari\,\ Joe\ Decuir\ -\ Sears\)\ \(CX2621\ -\ 99806\,\ 6-99806\,\ 49-75104\)\ ~.bin pong.bin
-fi
-if [ ! -e breakout.bin ]; then
-  ln -s ROMS/Breakout\ -\ Breakaway\ IV\ \(Paddle\)\ \(1978\)\ \(Atari\,\ Brad\ Stewart\ -\ Sears\)\ \(CX2622\ -\ 6-99813\,\ 49-75107\)\ ~.bin breakout.bin
-fi
-if [ ! -e seaquest.bin ]; then
-  ln -s ROMS/Seaquest\ \(1983\)\ \(Activision\,\ Steve\ Cartwright\)\ \(AX-022\)\ ~.bin seaquest.bin
-fi
