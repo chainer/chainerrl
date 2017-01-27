Fix computation of correction weight

chainerrl/agents/acer.py
@@ -188,10 +188,14 @@ class DiscreteACER(agent.AsyncAgent):
             if self.process_idx == 0:
                 logger.debug('t:%s s:%s v:%s Q_ret:%s',
                              i, states[i].sum(), v.data, Q_ret)
-            advantage = Q_ret - v
             log_prob = action_log_probs[i]
             entropy = action_entropy[i]
             action_distrib = action_distribs[i]
+            ba = np.expand_dims(actions[i], 0)
+
+            with chainer.no_backprop_mode():
+                Q = float(action_values[i].evaluate_actions(ba).data)
+                advantage = float((Q_ret - v).data)
 
             # Compute gradients w.r.t statistics produced by the model
             with backprop_truncated(action_distrib.logits):
@@ -199,21 +203,22 @@ class DiscreteACER(agent.AsyncAgent):
                 # Compute g
                 if rho is not None:
                     # Off-policy
-                    rho_bar = min(self.trust_region_c, rho[i])
                     g_loss = 0
-                    g_loss += rho_bar * log_prob * float(advantage.data)
-                    correction_weight = np.maximum(
-                        (rho_a[i] - self.trust_region_c) / rho_a[i],
-                        np.zeros_like(rho_a[i]))
+                    g_loss += (min(self.trust_region_c, rho[i]) *
+                               log_prob * advantage)
                     with chainer.no_backprop_mode():
-                        correction_advantage = (action_values[i].q_values.data -
-                                                values[i].data)
+                        correction_weight = (
+                            np.maximum(
+                                1 - self.trust_region_c / rho_a[i],
+                                np.zeros_like(rho_a[i])) *
+                            action_distrib.all_prob.data)
+                        correction_advantage = Q - float(values[i].data)
                     g_loss += F.sum(correction_weight *
-                                    action_distribs[i].all_log_prob *
+                                    action_distrib.all_log_prob *
                                     correction_advantage, axis=1)
                 else:
                     # On-policy
-                    g_loss = log_prob * float(advantage.data)
+                    g_loss = log_prob * advantage
                 g_loss.backward()
                 g = action_distrib.logits.grad[0]
                 action_distrib.logits.grad = None
@@ -236,8 +241,6 @@ class DiscreteACER(agent.AsyncAgent):
             # Accumulate gradients of value function
             v_loss += (Q_ret - v) ** 2 / 2
 
-            ba = np.expand_dims(actions[i], 0)
-            Q = float(action_values[i].evaluate_actions(ba).data)
             Q_ret = min(1, rho[i]) * (Q_ret - Q) + float(values[i].data)
 
         pi_loss *= self.pi_loss_coef