c2f3e379a2c4f0888bf52188937d5af44c7d1d36
muupan <muupan@gmail.com>
2017-02-01 16:22:30 +0900

    Fill chainerrl/__init__.py
    so that code like below will work:
    
    import chainerrl
    q_func = chainerrl.FCSAQFunction(...

---
+++ chainerrl/__init__.py
@@ -0,0 +1,23 @@
+from chainerrl import action_value  # NOQA
+from chainerrl import agent  # NOQA
+from chainerrl import agents  # NOQA
+from chainerrl import distribution  # NOQA
+from chainerrl import env  # NOQA
+from chainerrl import envs  # NOQA
+from chainerrl import experiments  # NOQA
+from chainerrl import explorer  # NOQA
+from chainerrl import explorers  # NOQA
+from chainerrl import explorers  # NOQA
+from chainerrl import functions  # NOQA
+from chainerrl import links  # NOQA
+from chainerrl import misc  # NOQA
+from chainerrl import optimizers  # NOQA
+from chainerrl import policies  # NOQA
+from chainerrl import policy  # NOQA
+from chainerrl import q_function  # NOQA
+from chainerrl import q_functions  # NOQA
+from chainerrl import recurrent  # NOQA
+from chainerrl import replay_buffer  # NOQA
+from chainerrl import v_function  # NOQA
+from chainerrl import v_functions  # NOQA
+
