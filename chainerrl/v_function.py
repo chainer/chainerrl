10794b81a0489d8c926cfdba12953c1936b166c4
muupan <muupan@gmail.com>
2017-02-01 11:54:07 +0900

    Move VFunction impl. to v_functions dir

--- chainerrl/v_function.py
+++ chainerrl/v_function.py
@@ -9,11 +9,7 @@ standard_library.install_aliases()
 from abc import ABCMeta
 from abc import abstractmethod
 
-import chainer
 from future.utils import with_metaclass
-
-from chainerrl.links.mlp import MLP
-from chainerrl.recurrent import RecurrentChainMixin
 
 
 class VFunction(with_metaclass(ABCMeta, object)):
@@ -22,33 +18,4 @@ class VFunction(with_metaclass(ABCMeta, object)):
     def __call__(self, x, test=False):
         raise NotImplementedError()
 
-
-class SingleModelVFunction(
-        chainer.Chain, VFunction, RecurrentChainMixin):
-    """Q-function with discrete actions.
-
-    Args:
-        model (chainer.Link):
-            Link that is callable and outputs action values.
-    """
-
-    def __init__(self, model):
-        super().__init__(model=model)
-
-    def __call__(self, x, test=False):
-        h = self.model(x, test=test)
-        return h
-
-
-class FCVFunction(SingleModelVFunction):
-
-    def __init__(self, n_input_channels, n_hidden_layers=0,
-                 n_hidden_channels=None):
-        self.n_input_channels = n_input_channels
-        self.n_hidden_layers = n_hidden_layers
-        self.n_hidden_channels = n_hidden_channels
-
-        super().__init__(
-            model=MLP(self.n_input_channels, 1,
-                      [self.n_hidden_channels] * self.n_hidden_layers),
-        )
+from chainerrl.v_functions import *  # NOQA
