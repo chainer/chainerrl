62db3f57b2b333ff9f19ed24df465379a3b5bf60
muupan <muupan@gmail.com>
2017-02-01 10:19:24 +0900

    Pass all the kwargs if a layer has var args
    The previous behaviour would not pass kwargs if a layer itself is Sequence.

--- chainerrl/links/sequence.py
+++ chainerrl/links/sequence.py
@@ -12,7 +12,16 @@ import funcsigs
 from chainerrl.recurrent import RecurrentChainMixin
 
 
+def accept_variable_arguments(func):
+    for param in funcsigs.signature(func).parameters.values():
+        if param.kind in (funcsigs.Parameter.VAR_POSITIONAL,
+                          funcsigs.Parameter.VAR_KEYWORD):
+            return True
+    return False
+
+
 class Sequence(chainer.ChainList, RecurrentChainMixin):
+    """Sequential callable Link that consists of other Links."""
 
     def __init__(self, *layers):
         self.layers = layers
@@ -20,12 +29,19 @@ class Sequence(chainer.ChainList, RecurrentChainMixin):
         # Cache the signatures because it might be slow
         self.argnames = [set(funcsigs.signature(layer).parameters)
                          for layer in layers]
+        self.accept_var_args = [accept_variable_arguments(layer)
+                                for layer in layers]
         super().__init__(*links)
 
     def __call__(self, x, **kwargs):
         h = x
-        for layer, argnames in zip(self.layers, self.argnames):
-            layer_kwargs = {k: v for k, v in kwargs.items()
-                            if k in argnames}
+        for layer, argnames, accept_var_args in zip(self.layers,
+                                                    self.argnames,
+                                                    self.accept_var_args):
+            if accept_var_args:
+                layer_kwargs = kwargs
+            else:
+                layer_kwargs = {k: v for k, v in kwargs.items()
+                                if k in argnames}
             h = layer(h, **layer_kwargs)
         return h
