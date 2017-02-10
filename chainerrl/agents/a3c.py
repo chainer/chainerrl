ce6a1adcd6bfbcc6f393c830a2a6e4e61fb427b5
Yuta Kikuchi <kikuchi@preferred.jp>
2017-02-10 18:48:44 +0900

    make prepare state and add as argument of __init__

--- chainerrl/agents/a3c.py
+++ chainerrl/agents/a3c.py
@@ -23,6 +23,10 @@ from chainerrl.recurrent import state_kept
 logger = getLogger(__name__)
 
 
+def prepare_state(state, phi):
+    return F.reshape(phi(state), (1, -1))
+
+
 class A3CModel(chainer.Link):
     """A3C model."""
 
@@ -104,7 +108,8 @@ class A3C(agent.AttributeSavingMixin, agent.AsyncAgent):
                  use_average_reward=False, average_reward_tau=1e-2,
                  act_deterministically=False,
                  average_entropy_decay=0.999,
-                 average_value_decay=0.999):
+                 average_value_decay=0.999,
+                 prepare_state=prepare_state):
 
         assert isinstance(model, A3CModel)
         # Globally shared model
@@ -130,6 +135,7 @@ class A3C(agent.AttributeSavingMixin, agent.AsyncAgent):
         self.act_deterministically = act_deterministically
         self.average_value_decay = average_value_decay
         self.average_entropy_decay = average_entropy_decay
+        self.prepare_state = prepare_state
 
         self.t = 0
         self.t_start = 0
@@ -172,9 +178,6 @@ class A3C(agent.AttributeSavingMixin, agent.AsyncAgent):
             if self.use_average_reward:
                 R -= self.average_reward
             v = self.past_values[i]
-            if self.process_idx == 0:
-                logger.debug('t:%s s:%s v:%s R:%s',
-                             i, self.past_states[i].data.sum(), v.data, R)
             advantage = R - v
             if self.use_average_reward:
                 self.average_reward += self.average_reward_tau * \
@@ -245,7 +248,7 @@ class A3C(agent.AttributeSavingMixin, agent.AsyncAgent):
         if self.clip_reward:
             reward = np.clip(reward, -1, 1)
 
-        statevar = chainer.Variable(np.expand_dims(self.phi(state), 0))
+        statevar = self.prepare_state(state, self.phi)
 
         self.past_rewards[self.t - 1] = reward
 
@@ -276,7 +279,7 @@ class A3C(agent.AttributeSavingMixin, agent.AsyncAgent):
     def act(self, obs):
         # Use the process-local model for acting
         with chainer.no_backprop_mode():
-            statevar = np.expand_dims(self.phi(obs), 0)
+            statevar = self.prepare_state(obs, self.phi)
             pout, _ = self.model.pi_and_v(statevar)
             if self.act_deterministically:
                 return pout.most_probable.data[0]
@@ -291,7 +294,7 @@ class A3C(agent.AttributeSavingMixin, agent.AsyncAgent):
         if done:
             self.update(None)
         else:
-            statevar = chainer.Variable(np.expand_dims(self.phi(state), 0))
+            statevar = self.prepare_state(state, self.phi)
             self.update(statevar)
 
         if isinstance(self.model, Recurrent):
