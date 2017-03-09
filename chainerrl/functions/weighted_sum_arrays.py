754b6af9226c88b2c64af4dd7d1fc53185315229
muupan <muupan@gmail.com>
2017-03-10 02:12:44 +0900

    Avoid error when len(xs) == 1

--- chainerrl/functions/weighted_sum_arrays.py
+++ chainerrl/functions/weighted_sum_arrays.py
@@ -13,8 +13,6 @@ class WeightedSumArrays(function.Function):
     def check_type_forward(self, in_types):
         type_check.expect(
             in_types[0].dtype.kind == 'f',
-            in_types[0].dtype == in_types[1].dtype,
-            in_types[0].shape == in_types[1].shape
         )
 
     def forward_cpu(self, inputs):
