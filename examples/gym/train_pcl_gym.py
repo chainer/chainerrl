2c9095b74d98a4d64c38f65578c171f7dd2e2ded
muupan <muupan@gmail.com>
2017-03-13 19:20:06 +0900

    Improve PCL example

--- examples/gym/train_pcl_gym.py
+++ examples/gym/train_pcl_gym.py
@@ -1,3 +1,14 @@
+"""An example of training PCL against OpenAI Gym Envs.
+
+This script is an example of training a PCL agent against OpenAI Gym envs.
+Both discrete and continuous action spaces are supported.
+
+To solve CartPole-v0, run:
+    python train_pcl_gym.py
+
+To solve InvertedPendulum-v1, run:
+    python train_pcl_gym.py --env InvertedPendulum-v1
+"""
 from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
@@ -8,76 +19,14 @@ standard_library.install_aliases()
 import argparse
 
 import chainer
-from chainer import functions as F
-from chainer import links as L
 import gym
 import gym.wrappers
 import numpy as np
 
 import chainerrl
-from chainerrl.agents import a3c
 from chainerrl import experiments
-from chainerrl import links
 from chainerrl import misc
-from chainerrl.optimizers.nonbias_weight_decay import NonbiasWeightDecay
 from chainerrl.optimizers import rmsprop_async
-from chainerrl import policies
-from chainerrl.recurrent import RecurrentChainMixin
-from chainerrl import v_function
-
-
-def phi(obs):
-    return obs.astype(np.float32)
-
-
-class A3CFFSoftmax(chainer.ChainList, a3c.A3CModel):
-
-    def __init__(self, ndim_obs, n_actions, hidden_sizes=(50, 50)):
-        self.pi = policies.SoftmaxPolicy(
-            model=links.MLP(ndim_obs, n_actions, hidden_sizes))
-        self.v = links.MLP(ndim_obs, 1, hidden_sizes=hidden_sizes)
-        super().__init__(self.pi, self.v)
-
-    def pi_and_v(self, state):
-        return self.pi(state), self.v(state)
-
-
-class A3CFFMellowmax(chainer.ChainList, a3c.A3CModel):
-
-    def __init__(self, ndim_obs, n_actions, hidden_sizes=(200, 200)):
-        self.pi = policies.MellowmaxPolicy(
-            model=links.MLP(ndim_obs, n_actions, hidden_sizes))
-        self.v = links.MLP(ndim_obs, 1, hidden_sizes=hidden_sizes)
-        super().__init__(self.pi, self.v)
-
-    def pi_and_v(self, state):
-        return self.pi(state), self.v(state)
-
-
-class A3CLSTMGaussian(chainer.ChainList, a3c.A3CModel, RecurrentChainMixin):
-
-    def __init__(self, obs_size, action_size, hidden_size=50, lstm_size=50):
-        self.pi_head = L.Linear(obs_size, hidden_size)
-        self.v_head = L.Linear(obs_size, hidden_size)
-        self.pi_lstm = L.LSTM(hidden_size, lstm_size)
-        self.v_lstm = L.LSTM(hidden_size, lstm_size)
-        self.pi = policies.LinearGaussianPolicyWithDiagonalCovariance(
-            lstm_size, action_size)
-        self.v = v_function.FCVFunction(lstm_size)
-        super().__init__(self.pi_head, self.v_head,
-                         self.pi_lstm, self.v_lstm, self.pi, self.v)
-
-    def pi_and_v(self, state):
-
-        def forward(head, lstm, tail):
-            h = F.relu(head(state))
-            # h = lstm(h)
-            return tail(h)
-
-        pout = forward(self.pi_head, self.pi_lstm, self.pi)
-        vout = forward(self.v_head, self.v_lstm, self.v)
-
-        return pout, vout
 
 
 def main():
@@ -87,8 +36,6 @@ def main():
     parser.add_argument('--processes', type=int, default=8)
     parser.add_argument('--gpu', type=int, default=0)
     parser.add_argument('--env', type=str, default='CartPole-v0')
-    parser.add_argument('--arch', type=str, default='FFSoftmax',
-                        choices=('FFSoftmax', 'FFMellowmax', 'LSTMGaussian'))
     parser.add_argument('--seed', type=int, default=None)
     parser.add_argument('--outdir', type=str, default=None)
     parser.add_argument('--batchsize', type=int, default=10)
@@ -96,18 +43,16 @@ def main():
     parser.add_argument('--n-hidden-channels', type=int, default=100)
     parser.add_argument('--n-hidden-layers', type=int, default=2)
     parser.add_argument('--n-times-replay', type=int, default=1)
-    parser.add_argument('--replay-start-size', type=int, default=2000)
-    parser.add_argument('--t-max', type=int, default=5)
+    parser.add_argument('--replay-start-size', type=int, default=10000)
+    parser.add_argument('--t-max', type=int, default=None)
     parser.add_argument('--tau', type=float, default=1e-2)
     parser.add_argument('--profile', action='store_true')
     parser.add_argument('--steps', type=int, default=8 * 10 ** 7)
     parser.add_argument('--eval-frequency', type=int, default=10 ** 5)
     parser.add_argument('--eval-n-runs', type=int, default=10)
     parser.add_argument('--reward-scale-factor', type=float, default=1e-2)
-    parser.add_argument('--rmsprop-epsilon', type=float, default=1e-1)
     parser.add_argument('--render', action='store_true', default=False)
     parser.add_argument('--lr', type=float, default=7e-4)
-    parser.add_argument('--weight-decay', type=float, default=0.0)
     parser.add_argument('--demo', action='store_true', default=False)
     parser.add_argument('--load', type=str, default='')
     parser.add_argument('--logger-level', type=int, default=logging.DEBUG)
@@ -116,7 +61,9 @@ def main():
     parser.add_argument('--disable-online-update', action='store_true',
                         default=False)
     parser.add_argument('--backprop-future-values', action='store_true',
-                        default=False)
+                        default=True)
+    parser.add_argument('--no-backprop-future-values', action='store_false',
+                        dest='backprop_future_values')
     args = parser.parse_args()
 
     logging.getLogger().setLevel(args.logger_level)
@@ -146,8 +93,7 @@ def main():
 
     # Switch policy types accordingly to action space types
     if isinstance(action_space, gym.spaces.Box):
-        # model = A3CLSTMGaussian(obs_space.low.size, action_space.low.size)
-        model = a3c.A3CSeparateModel(
+        model = chainerrl.agents.pcl.PCLSeparateModel(
             pi=chainerrl.policies.FCGaussianPolicy(
                 obs_space.low.size, action_space.low.size,
                 n_hidden_channels=args.n_hidden_channels,
@@ -158,43 +104,46 @@ def main():
                 var_wscale=1e-3,
                 var_bias=1,
                 var_type='diagonal',
-                # nonlinearity=F.elu,
-                # min_var=1e-2,
             ),
             v=chainerrl.v_functions.FCVFunction(
                 obs_space.low.size,
                 n_hidden_channels=args.n_hidden_channels,
                 n_hidden_layers=args.n_hidden_layers,
-                # nonlinearity=F.elu,
             )
         )
     else:
-        model = A3CFFSoftmax(obs_space.low.size, action_space.n)
+        model = chainerrl.agents.pcl.PCLSeparateModel(
+            pi=chainerrl.policies.FCSoftmaxPolicy(
+                obs_space.low.size, action_space.n,
+                n_hidden_channels=args.n_hidden_channels,
+                n_hidden_layers=args.n_hidden_layers
+            ),
+            v=chainerrl.v_functions.FCVFunction(
+                obs_space.low.size,
+                n_hidden_channels=args.n_hidden_channels,
+                n_hidden_layers=args.n_hidden_layers,
+            ),
+        )
+
     if not args.train_async and args.gpu >= 0:
         model.to_gpu(args.gpu)
 
-    opt = rmsprop_async.RMSpropAsync(
-        lr=args.lr, eps=args.rmsprop_epsilon, alpha=0.99)
+    if args.train_async:
+        opt = rmsprop_async.RMSpropAsync(lr=args.lr, alpha=0.99)
+    else:
+        opt = chainer.optimizers.Adam(alpha=args.lr)
     opt.setup(model)
-    opt.add_hook(chainer.optimizer.GradientClipping(40))
-    if args.weight_decay > 0:
-        opt.add_hook(NonbiasWeightDecay(args.weight_decay))
 
-    # agent = a3c.A3C(model, opt, t_max=args.t_max, gamma=0.99,
-    #                 beta=args.beta, phi=phi)
     replay_buffer = chainerrl.replay_buffer.EpisodicReplayBuffer(10 ** 5)
-    # explorer = chainerrl.explorers.AdditiveOU(start_with_mu=True)
-    explorer = None
     agent = chainerrl.agents.PCL(
         model, opt, replay_buffer=replay_buffer,
         t_max=args.t_max, gamma=0.99,
         tau=args.tau,
-        phi=phi,
+        phi=lambda x: x.astype(np.float32, copy=False),
         rollout_len=args.rollout_len,
         n_times_replay=args.n_times_replay,
         replay_start_size=args.replay_start_size,
         batchsize=args.batchsize,
-        explorer=explorer,
         train_async=args.train_async,
         disable_online_update=args.disable_online_update,
         backprop_future_values=args.backprop_future_values,
