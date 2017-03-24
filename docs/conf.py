c04a71cbdb8047023e2c5f8eef6f1c1c9e915faf
muupan <muupan@gmail.com>
2017-03-24 17:18:30 +0900

    Add napoleon to parse docstrings correctly

--- docs/conf.py
+++ docs/conf.py
@@ -37,6 +37,7 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
+    'sphinx.ext.napoleon',
     'sphinx.ext.viewcode']
 
 # Add any paths that contain templates here, relative to this directory.
