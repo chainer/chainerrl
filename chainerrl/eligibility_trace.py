9589271cacfb699c62c445cbe67abeee4ea48e08
Toshiki Kataoka <kataoka@preferred.jp>
2017-04-27 14:18:20 +0900

    lambda return

--- chainerrl/eligibility_trace.py
+++ chainerrl/eligibility_trace.py
@@ -1,11 +1,24 @@
 import numpy as np
 
 
+def lambda_return(lambd, rewards, next_values, gamma, R=0.0):
+    n = rewards.size
+    ret = np.zeros(n)
+    for i in reversed(range(n)):
+        # R = lambd * (rewards[i] + gamma * R) \
+        #     + (1 - lambd) * (rewards[i] + gamma * next_values[i])
+        R = lambd * R + (1 - lambd) * next_values[i]
+        R = rewards[i] + gamma * R
+        ret[i] = R
+    return ret
+
+
 def retrace(Q, rewards, values, gamma, likelihood_ratio, R=0.0, lambd=1.0):
     return general_trace(
         Q, rewards, values, gamma,
         np.clip(likelihood_ratio, None, lambd),
         R=R)
+
 
 def general_trace(Q, rewards, values, gamma, correction_coefs, R=0.0):
     n = Q.size
