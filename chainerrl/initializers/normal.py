bf04a2f476b427fd2595a0e08d6aa02c6b933eb4
Toshiki Kataoka <kataoka@preferred.jp>
2017-06-02 12:34:58 +0900

    comment

--- chainerrl/initializers/normal.py
+++ chainerrl/initializers/normal.py
@@ -3,8 +3,10 @@ import numpy as np
 
 
 class LeCunNormal(chainer.initializers.HeNormal):
-    """sorry
+    """LeCunNormal is (essentially) the default initializer in Chainer v1.
 
+    chainer.initializers.LeCunNormal is not available yet.
+    (Chainer Pull Request #2764 has not been merged.)
     """
     def __init__(self, scale=1.0, dtype=None):
         super(LeCunNormal, self).__init__(np.sqrt(0.5)*scale, dtype)
