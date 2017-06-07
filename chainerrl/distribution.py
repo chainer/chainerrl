Fix the sign in Gumbel-max trick

chainerrl/distribution.py
@@ -37,13 +37,13 @@ def sample_discrete_actions(batch_probs):
     """Sample a batch of actions from a batch of action probabilities.
 
     Args:
-      batch_probs (ndarray): batch of action probabilities BxA
+        batch_probs (ndarray): batch of action probabilities BxA
     Returns:
-      List consisting of sampled actions
+        ndarray consisting of sampled action indices
     """
     xp = chainer.cuda.get_array_module(batch_probs)
     return xp.argmax(
-        xp.log(batch_probs) - xp.random.gumbel(size=batch_probs.shape),
+        xp.log(batch_probs) + xp.random.gumbel(size=batch_probs.shape),
         axis=1).astype(np.int32, copy=False)
 
 

tests/test_distribution.py
@@ -36,7 +36,7 @@ class TestSampleDiscreteActions(unittest.TestCase):
             for i in range(batch_probs.shape[0]):
                 counter[i][batch_indices[i]] += 1
         np.testing.assert_allclose(
-            counter / 1000, chainer.cuda.to_cpu(batch_probs), atol=0.1)
+            counter / 1000, chainer.cuda.to_cpu(batch_probs), atol=0.05)
 
     @testing.condition.retry(3)
     def test_cpu(self):