4d0e81e560e5d731649ee75c9fe8eddf4018e5a3
Toshiki Kataoka <kataoka@preferred.jp>
2017-08-21 19:03:54 +0900

    Python 3 features

--- chainerrl/agents/ppo.py
+++ chainerrl/agents/ppo.py
@@ -1,3 +1,11 @@
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+from __future__ import absolute_import
+from builtins import *  # NOQA
+from future import standard_library
+standard_library.install_aliases()
+
 import chainer
 from chainer import cuda
 import chainer.functions as F
