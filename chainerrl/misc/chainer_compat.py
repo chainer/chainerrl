25849f300aad8786edbb5644742396d8ae79963e
Toshiki Kataoka <kataoka@preferred.jp>
2017-10-17 13:45:54 +0900

    add comment

--- chainerrl/misc/chainer_compat.py
+++ chainerrl/misc/chainer_compat.py
@@ -8,6 +8,10 @@ chainer_version = StrictVersion(
     pkg_resources.get_distribution("chainer").version)
 
 if chainer_version < StrictVersion('3.0.0a1'):
+    """Chainer's PR #2426 changed the behavior of matmul
+
+    Simulate the newer behavior by functions in Chainer v2
+    """
     def matmul_v3(a, b, **kwargs):
         if (a.ndim, b.ndim) == (3, 3):
             return F.batch_matmul(a, b, **kwargs)
