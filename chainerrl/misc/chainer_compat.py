2b70885f77944e8adabb227318afffd5e0e49f81
Toshiki Kataoka <kataoka@preferred.jp>
2017-10-17 17:15:40 +0900

    Use chainer.__version__

--- chainerrl/misc/chainer_compat.py
+++ chainerrl/misc/chainer_compat.py
@@ -1,11 +1,10 @@
 from distutils.version import StrictVersion
-import pkg_resources
 
+import chainer
 import chainer.functions as F
 
 
-chainer_version = StrictVersion(
-    pkg_resources.get_distribution("chainer").version)
+chainer_version = StrictVersion(chainer.__version__)
 
 if chainer_version < StrictVersion('3.0.0a1'):
     """Chainer's PR #2426 changed the behavior of matmul
