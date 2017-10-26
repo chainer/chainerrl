c6986ba7ac7997710125b8637876c6d6225c7444
Toshiki Kataoka <kataoka@preferred.jp>
2017-10-26 13:24:03 +0900

    split tests

--- tests/links_tests/test_empirical_normalization.py
+++ tests/links_tests/test_empirical_normalization.py
@@ -1,16 +1,17 @@
 import unittest
 
+from chainer import testing
 import numpy as np
 
 from chainerrl.links import empirical_normalization
 
 
 class TestEmpiricalNormalization(unittest.TestCase):
-    def test(self):
+    def test_small(self):
         en = empirical_normalization.EmpiricalNormalization(10)
         xs = []
-        for _ in range(10000):
-            x = np.random.normal(loc=4, scale=2, size=(7, 10))
+        for t in range(10):
+            x = np.random.normal(loc=4, scale=2, size=(t+3, 10))
             en(x)
             xs.extend(list(x))
         xs = np.array(xs)
@@ -19,16 +20,22 @@ class TestEmpiricalNormalization(unittest.TestCase):
         np.testing.assert_allclose(en.mean, true_mean, rtol=1e-4)
         np.testing.assert_allclose(np.sqrt(en.var), true_std, rtol=1e-4)
 
+    @testing.attr.slow
+    def test_large(self):
+        en = empirical_normalization.EmpiricalNormalization(10)
+        for _ in range(10000):
+            x = np.random.normal(loc=4, scale=2, size=(7, 10))
+            en(x)
         x = 2 * np.random.normal(loc=4, scale=2, size=(1, 10))
         enx = en(x)
         # mean, std = en.mean_and_std()
         mean = en.mean
         std = np.sqrt(en.var)
-        print('mean', mean)
         np.testing.assert_allclose(mean, np.full_like(mean, 4), rtol=1e-1)
-        print('std', std)
         np.testing.assert_allclose(std, np.full_like(std, 2), rtol=1e-1)
-        print('ground-truth normaliaztion', (x - 4) / 2)
-        print('en(x)', enx)
+
+        # Compare with the ground-truth normalization
         np.testing.assert_allclose((x - 4) / 2, enx, rtol=1e-1)
+
+        # Test inverse
         np.testing.assert_allclose(x, en.inverse(enx), rtol=1e-1)
