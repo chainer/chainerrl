c054076eff4910a6190d4d4c9e80a07d9d078532
muupan <muupan@gmail.com>
2017-11-07 15:30:40 +0900

    Add CastObservation and CastObservationToFloat32

---
+++ chainerrl/wrappers/__init__.py
@@ -0,0 +1,2 @@
+from chainerrl.wrappers.cast_observation import CastObservation  # NOQA
+from chainerrl.wrappers.cast_observation import CastObservationToFloat32  # NOQA
---
+++ chainerrl/wrappers/cast_observation.py
@@ -0,0 +1,38 @@
+from __future__ import print_function
+from __future__ import unicode_literals
+from __future__ import division
+from __future__ import absolute_import
+from builtins import *  # NOQA
+from future import standard_library
+standard_library.install_aliases()
+
+import gym
+import numpy as np
+
+
+class CastObservation(gym.ObservationWrapper):
+    """Cast observations to a given type.
+
+    Args:
+        env: Env to wrap.
+        dtype: Data type object.
+    """
+
+    def __init__(self, env, dtype):
+        super().__init__(env)
+        self.dtype = dtype
+
+    def _observation(self, observation):
+        self.original_observation = observation
+        return observation.astype(self.dtype, copy=False)
+
+
+class CastObservationToFloat32(CastObservation):
+    """Cast observations to float32, which is common in Chainer.
+
+    Args:
+        env: Env to wrap.
+    """
+
+    def __init__(self, env):
+        super().__init__(env, np.float32)
