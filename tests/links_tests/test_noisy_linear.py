41c3e7d86b90c13943577c92de6817ce83ca4cf5
Toshiki Kataoka <kataoka@preferred.jp>
2017-11-14 15:19:54 +0900

    Test randomness

--- tests/links_tests/test_noisy_linear.py
+++ tests/links_tests/test_noisy_linear.py
@@ -16,11 +16,29 @@ from chainerrl.links import noisy_linear
     'nobias': [False, True],
 }))
 class TestFactorizedNoisyLinear(unittest.TestCase):
-    def test_calls(self):
-        l = noisy_linear.FactorizedNoisyLinear(
+    def setUp(self):
+        self.l = noisy_linear.FactorizedNoisyLinear(
             *self.size_args, nobias=self.nobias)
+
+    def test_calls(self):
         x_data = np.arange(12).astype(np.float32).reshape((2, 6))
         x = chainer.Variable(x_data)
-        l(x)
-        l(x_data + 1)
-        l(x_data.reshape((2, 3, 2)))
+        self.l(x)
+        self.l(x_data + 1)
+        self.l(x_data.reshape((2, 3, 2)))
+
+    @testing.condition.retry(3)
+    def test_randomness(self):
+        x = np.random.standard_normal((10, 6)).astype(np.float32)
+        y1 = self.l(x).data
+        y2 = self.l(x).data
+        d = np.mean(np.sqrt(np.mean(np.square(y1 - y2), axis=1)))
+
+        # d should be approx to sigma_scale = 0.4.
+        # Note: This approximation is not exact.
+        target = 0.4
+        if not self.nobias:
+            target *= np.sqrt(2)
+
+        self.assertGreater(d, target / 3.)
+        self.assertLess(d, target * 3.)
