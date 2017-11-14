3059dd810749ef0214b86695449b1075aa91bf6b
Toshiki Kataoka <kataoka@preferred.jp>
2017-11-14 14:54:00 +0900

    Use parametrized test

--- tests/links_tests/test_noisy_linear.py
+++ tests/links_tests/test_noisy_linear.py
@@ -8,19 +8,19 @@ from chainerrl.links import noisy_linear
 
 
 @testing.parameterize(*testing.product({
+    'size_args': [
+        (5,),  # uninitialized from Chainer v2
+        (None, 5),  # uninitialized
+        (6, 5),  # initialized
+    ],
     'nobias': [False, True],
 }))
 class TestFactorizedNoisyLinear(unittest.TestCase):
     def test_calls(self):
-        l_uninitialized_new = noisy_linear.FactorizedNoisyLinear(
-            5, nobias=self.nobias)
-        l_uninitialized_old = noisy_linear.FactorizedNoisyLinear(
-            None, 5, nobias=self.nobias)
-        l_initialized = noisy_linear.FactorizedNoisyLinear(
-            6, 5, nobias=self.nobias)
-        for l in [l_uninitialized_new, l_uninitialized_old, l_initialized]:
-            x_data = np.arange(12).astype(np.float32).reshape((2, 6))
-            x = chainer.Variable(x_data)
-            l(x)
-            l(x_data + 1)
-            l(x_data.reshape((2, 3, 2)))
+        l = noisy_linear.FactorizedNoisyLinear(
+            *self.size_args, nobias=self.nobias)
+        x_data = np.arange(12).astype(np.float32).reshape((2, 6))
+        x = chainer.Variable(x_data)
+        l(x)
+        l(x_data + 1)
+        l(x_data.reshape((2, 3, 2)))
