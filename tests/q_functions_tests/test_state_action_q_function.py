38e4d1a3b78d6cac37864ae96aaeceab17d556bf
muupan <muupan@gmail.com>
2017-11-15 12:56:10 +0900

    Avoid duplicated code

--- tests/q_functions_tests/test_state_action_q_function.py
+++ tests/q_functions_tests/test_state_action_q_function.py
@@ -18,6 +18,26 @@ from chainer.testing import attr
 import chainerrl
 
 
+class _TestSAQFunction(unittest.TestCase):
+
+    def _test_call_given_model(self, model, gpu):
+        # This method only check if a given model can receive random input
+        # data and return output data with the correct interface.
+        batch_size = 7
+        obs = np.random.rand(batch_size, self.n_dim_obs).astype(np.float32)
+        action = np.random.rand(
+            batch_size, self.n_dim_action).astype(np.float32)
+        if gpu >= 0:
+            model.to_gpu(gpu)
+            obs = chainer.cuda.to_gpu(obs)
+            action = chainer.cuda.to_gpu(action)
+        y = model(obs, action)
+        self.assertTrue(isinstance(y, chainer.Variable))
+        self.assertEqual(y.shape, (batch_size, 1))
+        self.assertEqual(chainer.cuda.get_array_module(y),
+                         chainer.cuda.get_array_module(obs))
+
+
 @testing.parameterize(
     *testing.product({
         'n_dim_obs': [1, 5],
@@ -28,11 +48,11 @@ import chainerrl
         'last_wscale': [1, 1e-3],
     })
 )
-class TestFCSAQFunction(unittest.TestCase):
+class TestFCSAQFunction(_TestSAQFunction):
 
     def _test_call(self, gpu):
         nonlinearity = getattr(F, self.nonlinearity)
-        qf = chainerrl.q_functions.FCSAQFunction(
+        model = chainerrl.q_functions.FCSAQFunction(
             n_dim_obs=self.n_dim_obs,
             n_dim_action=self.n_dim_action,
             n_hidden_layers=self.n_hidden_layers,
@@ -40,19 +60,7 @@ class TestFCSAQFunction(unittest.TestCase):
             nonlinearity=nonlinearity,
             last_wscale=self.last_wscale,
         )
-        batch_size = 7
-        obs = np.random.rand(batch_size, self.n_dim_obs).astype(np.float32)
-        action = np.random.rand(
-            batch_size, self.n_dim_action).astype(np.float32)
-        if gpu >= 0:
-            qf.to_gpu(gpu)
-            obs = chainer.cuda.to_gpu(obs)
-            action = chainer.cuda.to_gpu(action)
-        y = qf(obs, action)
-        self.assertTrue(isinstance(y, chainer.Variable))
-        self.assertEqual(y.shape, (batch_size, 1))
-        self.assertEqual(chainer.cuda.get_array_module(y),
-                         chainer.cuda.get_array_module(obs))
+        self._test_call_given_model(model, gpu)
 
     def test_call_cpu(self):
         self._test_call(gpu=-1)
@@ -72,11 +80,11 @@ class TestFCSAQFunction(unittest.TestCase):
         'last_wscale': [1, 1e-3],
     })
 )
-class TestFCLSTMSAQFunction(unittest.TestCase):
+class TestFCLSTMSAQFunction(_TestSAQFunction):
 
     def _test_call(self, gpu):
         nonlinearity = getattr(F, self.nonlinearity)
-        qf = chainerrl.q_functions.FCLSTMSAQFunction(
+        model = chainerrl.q_functions.FCLSTMSAQFunction(
             n_dim_obs=self.n_dim_obs,
             n_dim_action=self.n_dim_action,
             n_hidden_layers=self.n_hidden_layers,
@@ -84,65 +92,7 @@ class TestFCLSTMSAQFunction(unittest.TestCase):
             nonlinearity=nonlinearity,
             last_wscale=self.last_wscale,
         )
-        batch_size = 7
-        obs = np.random.rand(batch_size, self.n_dim_obs).astype(np.float32)
-        action = np.random.rand(
-            batch_size, self.n_dim_action).astype(np.float32)
-        if gpu >= 0:
-            qf.to_gpu(gpu)
-            obs = chainer.cuda.to_gpu(obs)
-            action = chainer.cuda.to_gpu(action)
-        y = qf(obs, action)
-        self.assertTrue(isinstance(y, chainer.Variable))
-        self.assertEqual(y.shape, (batch_size, 1))
-        self.assertEqual(chainer.cuda.get_array_module(y),
-                         chainer.cuda.get_array_module(obs))
-
-    def test_call_cpu(self):
-        self._test_call(gpu=-1)
-
-    @attr.gpu
-    def test_call_gpu(self):
-        self._test_call(gpu=0)
-
-
-@testing.parameterize(
-    *testing.product({
-        'n_dim_obs': [1, 5],
-        'n_dim_action': [1, 3],
-        'n_hidden_layers': [0, 1, 2],
-        'n_hidden_channels': [1, 2],
-        'normalize_input': [True, False],
-        'nonlinearity': ['relu', 'elu'],
-        'last_wscale': [1, 1e-3],
-    })
-)
-class TestFCBNSAQFunction(unittest.TestCase):
-
-    def _test_call(self, gpu):
-        nonlinearity = getattr(F, self.nonlinearity)
-        qf = chainerrl.q_functions.FCBNSAQFunction(
-            n_dim_obs=self.n_dim_obs,
-            n_dim_action=self.n_dim_action,
-            n_hidden_layers=self.n_hidden_layers,
-            n_hidden_channels=self.n_hidden_channels,
-            normalize_input=self.normalize_input,
-            nonlinearity=nonlinearity,
-            last_wscale=self.last_wscale,
-        )
-        batch_size = 7
-        obs = np.random.rand(batch_size, self.n_dim_obs).astype(np.float32)
-        action = np.random.rand(
-            batch_size, self.n_dim_action).astype(np.float32)
-        if gpu >= 0:
-            qf.to_gpu(gpu)
-            obs = chainer.cuda.to_gpu(obs)
-            action = chainer.cuda.to_gpu(action)
-        y = qf(obs, action)
-        self.assertTrue(isinstance(y, chainer.Variable))
-        self.assertEqual(y.shape, (batch_size, 1))
-        self.assertEqual(chainer.cuda.get_array_module(y),
-                         chainer.cuda.get_array_module(obs))
+        self._test_call_given_model(model, gpu)
 
     def test_call_cpu(self):
         self._test_call(gpu=-1)
@@ -163,11 +113,11 @@ class TestFCBNSAQFunction(unittest.TestCase):
         'last_wscale': [1, 1e-3],
     })
 )
-class TestFCBNLateActionSAQFunction(unittest.TestCase):
+class TestFCBNSAQFunction(_TestSAQFunction):
 
     def _test_call(self, gpu):
         nonlinearity = getattr(F, self.nonlinearity)
-        qf = chainerrl.q_functions.FCBNLateActionSAQFunction(
+        model = chainerrl.q_functions.FCBNSAQFunction(
             n_dim_obs=self.n_dim_obs,
             n_dim_action=self.n_dim_action,
             n_hidden_layers=self.n_hidden_layers,
@@ -176,19 +126,41 @@ class TestFCBNLateActionSAQFunction(unittest.TestCase):
             nonlinearity=nonlinearity,
             last_wscale=self.last_wscale,
         )
-        batch_size = 7
-        obs = np.random.rand(batch_size, self.n_dim_obs).astype(np.float32)
-        action = np.random.rand(
-            batch_size, self.n_dim_action).astype(np.float32)
-        if gpu >= 0:
-            qf.to_gpu(gpu)
-            obs = chainer.cuda.to_gpu(obs)
-            action = chainer.cuda.to_gpu(action)
-        y = qf(obs, action)
-        self.assertTrue(isinstance(y, chainer.Variable))
-        self.assertEqual(y.shape, (batch_size, 1))
-        self.assertEqual(chainer.cuda.get_array_module(y),
-                         chainer.cuda.get_array_module(obs))
+        self._test_call_given_model(model, gpu)
+
+    def test_call_cpu(self):
+        self._test_call(gpu=-1)
+
+    @attr.gpu
+    def test_call_gpu(self):
+        self._test_call(gpu=0)
+
+
+@testing.parameterize(
+    *testing.product({
+        'n_dim_obs': [1, 5],
+        'n_dim_action': [1, 3],
+        'n_hidden_layers': [0, 1, 2],
+        'n_hidden_channels': [1, 2],
+        'normalize_input': [True, False],
+        'nonlinearity': ['relu', 'elu'],
+        'last_wscale': [1, 1e-3],
+    })
+)
+class TestFCBNLateActionSAQFunction(_TestSAQFunction):
+
+    def _test_call(self, gpu):
+        nonlinearity = getattr(F, self.nonlinearity)
+        model = chainerrl.q_functions.FCBNLateActionSAQFunction(
+            n_dim_obs=self.n_dim_obs,
+            n_dim_action=self.n_dim_action,
+            n_hidden_layers=self.n_hidden_layers,
+            n_hidden_channels=self.n_hidden_channels,
+            normalize_input=self.normalize_input,
+            nonlinearity=nonlinearity,
+            last_wscale=self.last_wscale,
+        )
+        self._test_call_given_model(model, gpu)
 
     def test_call_cpu(self):
         self._test_call(gpu=-1)
@@ -208,11 +180,11 @@ class TestFCBNLateActionSAQFunction(unittest.TestCase):
         'last_wscale': [1, 1e-3],
     })
 )
-class TestFCLateActionSAQFunction(unittest.TestCase):
+class TestFCLateActionSAQFunction(_TestSAQFunction):
 
     def _test_call(self, gpu):
         nonlinearity = getattr(F, self.nonlinearity)
-        qf = chainerrl.q_functions.FCLateActionSAQFunction(
+        model = chainerrl.q_functions.FCLateActionSAQFunction(
             n_dim_obs=self.n_dim_obs,
             n_dim_action=self.n_dim_action,
             n_hidden_layers=self.n_hidden_layers,
@@ -220,19 +192,7 @@ class TestFCLateActionSAQFunction(unittest.TestCase):
             nonlinearity=nonlinearity,
             last_wscale=self.last_wscale,
         )
-        batch_size = 7
-        obs = np.random.rand(batch_size, self.n_dim_obs).astype(np.float32)
-        action = np.random.rand(
-            batch_size, self.n_dim_action).astype(np.float32)
-        if gpu >= 0:
-            qf.to_gpu(gpu)
-            obs = chainer.cuda.to_gpu(obs)
-            action = chainer.cuda.to_gpu(action)
-        y = qf(obs, action)
-        self.assertTrue(isinstance(y, chainer.Variable))
-        self.assertEqual(y.shape, (batch_size, 1))
-        self.assertEqual(chainer.cuda.get_array_module(y),
-                         chainer.cuda.get_array_module(obs))
+        self._test_call_given_model(model, gpu)
 
     def test_call_cpu(self):
         self._test_call(gpu=-1)
