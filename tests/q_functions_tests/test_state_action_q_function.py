bcc85c230eb30d1d6cd19c097be55290021be8f7
muupan <muupan@gmail.com>
2017-11-16 17:59:10 +0900

    Remove test cases with n_hidden_layers=0 for LateAction arch

--- tests/q_functions_tests/test_state_action_q_function.py
+++ tests/q_functions_tests/test_state_action_q_function.py
@@ -139,7 +139,7 @@ class TestFCBNSAQFunction(_TestSAQFunction):
     *testing.product({
         'n_dim_obs': [1, 5],
         'n_dim_action': [1, 3],
-        'n_hidden_layers': [0, 1, 2],
+        'n_hidden_layers': [1, 2],  # LateAction requires n_hidden_layers >=1
         'n_hidden_channels': [1, 2],
         'normalize_input': [True, False],
         'nonlinearity': ['relu', 'elu'],
@@ -173,7 +173,7 @@ class TestFCBNLateActionSAQFunction(_TestSAQFunction):
     *testing.product({
         'n_dim_obs': [1, 5],
         'n_dim_action': [1, 3],
-        'n_hidden_layers': [0, 1, 2],
+        'n_hidden_layers': [1, 2],  # LateAction requires n_hidden_layers >=1
         'n_hidden_channels': [1, 2],
         'nonlinearity': ['relu', 'elu'],
         'last_wscale': [1, 1e-3],
