dc9eff7a0111dab90b488de95558c5b7393a4277
Toshiki Kataoka <kataoka@preferred.jp>
2017-11-22 15:31:26 +0900

    Parameterize tests
    Stop recomputing min_action, max_action

--- tests/policies_tests/test_deterministic_policy.py
+++ tests/policies_tests/test_deterministic_policy.py
@@ -17,13 +17,63 @@ import numpy as np
 import chainerrl
 
 
-class _TestDeterministicPolicy(unittest.TestCase):
+@testing.parameterize(
+    *testing.product({
+        'n_input_channels': [1, 5],
+        'action_size': [1, 2],
+        'bound_action': [True, False],
+        'nonlinearity': ['relu', 'elu'],
+        'model_class': [chainerrl.policies.FCDeterministicPolicy],
+        'model_kwargs': testing.product({
+            'n_hidden_layers': [0, 1, 2],
+            'n_hidden_channels': [1, 2],
+            'last_wscale': [1, 1e-3],
+        }),
+    }),
+    *testing.product({
+        'n_input_channels': [1, 5],
+        'action_size': [1, 2],
+        'bound_action': [True, False],
+        'nonlinearity': ['relu', 'elu'],
+        'model_class': [chainerrl.policies.FCBNDeterministicPolicy],
+        'model_kwargs': testing.product({
+            'n_hidden_layers': [0, 1, 2],
+            'n_hidden_channels': [1, 2],
+            'normalize_input': [True, False],
+            'last_wscale': [1, 1e-3],
+        }),
+    }),
+    *testing.product({
+        'n_input_channels': [1, 5],
+        'action_size': [1, 2],
+        'bound_action': [True, False],
+        'nonlinearity': ['relu', 'elu'],
+        'model_class': [chainerrl.policies.FCLSTMDeterministicPolicy],
+        'model_kwargs': testing.product({
+            'n_hidden_layers': [0, 1, 2],
+            'n_hidden_channels': [1, 2],
+            'last_wscale': [1, 1e-3],
+        }),
+    })
+)
+class TestDeterministicPolicy(unittest.TestCase):
 
-    def _test_call_given_model(self, model, gpu):
+    def _test_call(self, gpu):
         # This method only check if a given model can receive random input
         # data and return output data with the correct interface.
+        nonlinearity = getattr(F, self.nonlinearity)
         min_action = np.full((self.action_size,), -0.01, dtype=np.float32)
         max_action = np.full((self.action_size,), 0.01, dtype=np.float32)
+        model = self.model_class(
+            n_input_channels=self.n_input_channels,
+            action_size=self.action_size,
+            bound_action=self.bound_action,
+            min_action=min_action,
+            max_action=max_action,
+            nonlinearity=nonlinearity,
+            **self.model_kwargs,
+        )
+
         batch_size = 7
         x = np.random.rand(
             batch_size, self.n_input_channels).astype(np.float32)
@@ -43,114 +93,6 @@ class _TestDeterministicPolicy(unittest.TestCase):
         if self.bound_action:
             self.assertTrue((a.data <= max_action).all())
             self.assertTrue((a.data >= min_action).all())
-
-
-@testing.parameterize(
-    *testing.product({
-        'n_input_channels': [1, 5],
-        'action_size': [1, 2],
-        'bound_action': [True, False],
-        'nonlinearity': ['relu', 'elu'],
-        'model_kwargs': testing.product({
-            'n_hidden_layers': [0, 1, 2],
-            'n_hidden_channels': [1, 2],
-            'last_wscale': [1, 1e-3],
-        }),
-    })
-)
-class TestFCDeterministicPolicy(_TestDeterministicPolicy):
-
-    def _test_call(self, gpu):
-        nonlinearity = getattr(F, self.nonlinearity)
-        min_action = np.full((self.action_size,), -0.01, dtype=np.float32)
-        max_action = np.full((self.action_size,), 0.01, dtype=np.float32)
-        model = chainerrl.policies.FCDeterministicPolicy(
-            n_input_channels=self.n_input_channels,
-            action_size=self.action_size,
-            bound_action=self.bound_action,
-            min_action=min_action,
-            max_action=max_action,
-            nonlinearity=nonlinearity,
-            **self.model_kwargs,
-        )
-        self._test_call_given_model(model, gpu)
-
-    def test_call_cpu(self):
-        self._test_call(gpu=-1)
-
-    @attr.gpu
-    def test_call_gpu(self):
-        self._test_call(gpu=0)
-
-
-@testing.parameterize(
-    *testing.product({
-        'n_input_channels': [1, 5],
-        'action_size': [1, 2],
-        'bound_action': [True, False],
-        'nonlinearity': ['relu', 'elu'],
-        'model_kwargs': testing.product({
-            'n_hidden_layers': [0, 1, 2],
-            'n_hidden_channels': [1, 2],
-            'normalize_input': [True, False],
-            'last_wscale': [1, 1e-3],
-        }),
-    })
-)
-class TestFCBNDeterministicPolicy(_TestDeterministicPolicy):
-
-    def _test_call(self, gpu):
-        nonlinearity = getattr(F, self.nonlinearity)
-        min_action = np.full((self.action_size,), -0.01, dtype=np.float32)
-        max_action = np.full((self.action_size,), 0.01, dtype=np.float32)
-        model = chainerrl.policies.FCBNDeterministicPolicy(
-            n_input_channels=self.n_input_channels,
-            action_size=self.action_size,
-            bound_action=self.bound_action,
-            min_action=min_action,
-            max_action=max_action,
-            nonlinearity=nonlinearity,
-            **self.model_kwargs,
-        )
-        self._test_call_given_model(model, gpu)
-
-    def test_call_cpu(self):
-        self._test_call(gpu=-1)
-
-    @attr.gpu
-    def test_call_gpu(self):
-        self._test_call(gpu=0)
-
-
-@testing.parameterize(
-    *testing.product({
-        'n_input_channels': [1, 5],
-        'action_size': [1, 2],
-        'bound_action': [True, False],
-        'nonlinearity': ['relu', 'elu'],
-        'model_kwargs': testing.product({
-            'n_hidden_layers': [0, 1, 2],
-            'n_hidden_channels': [1, 2],
-            'last_wscale': [1, 1e-3],
-        }),
-    })
-)
-class TestFCLSTMDeterministicPolicy(_TestDeterministicPolicy):
-
-    def _test_call(self, gpu):
-        nonlinearity = getattr(F, self.nonlinearity)
-        min_action = np.full((self.action_size,), -0.01, dtype=np.float32)
-        max_action = np.full((self.action_size,), 0.01, dtype=np.float32)
-        model = chainerrl.policies.FCLSTMDeterministicPolicy(
-            n_input_channels=self.n_input_channels,
-            action_size=self.action_size,
-            bound_action=self.bound_action,
-            min_action=min_action,
-            max_action=max_action,
-            nonlinearity=nonlinearity,
-            **self.model_kwargs,
-        )
-        self._test_call_given_model(model, gpu)
 
     def test_call_cpu(self):
         self._test_call(gpu=-1)
