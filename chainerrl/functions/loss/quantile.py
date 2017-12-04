ce059270e34f3558dfe25bd9841b258b1497693b
Toshiki Kataoka <kataoka@preferred.jp>
2017-12-04 20:55:34 +0900

    Add QuantileHuberLoss

--- chainerrl/functions/loss/quantile.py
+++ chainerrl/functions/loss/quantile.py
@@ -1,9 +1,11 @@
+from chainer import cuda
+from chainer import function
 import chainer.functions as F
+from chainer.utils import type_check
+import numpy
 
 
-def quantile_loss(y, t, tau, reduce='no'):
-    assert reduce == 'no', 'TODO'
-
+def quantile_loss(y, t, tau):
     e = t - y
     return F.maximum(
         e * tau,
@@ -14,7 +16,7 @@ def quantile_loss(y, t, tau, reduce='no'):
 def quantile_huber_loss_Aravkin(y, t, tau, delta=1.):
     """Quantile Huber loss a la Aravkin+
 
-    1402.4624
+    See: https://arxiv.org/abs/1402.4624
     """
     assert False
     e = t - y
@@ -27,11 +29,50 @@ def quantile_huber_loss_Aravkin(y, t, tau, delta=1.):
     )
 
 
+class QuantileHuberLossDabney(function.Function):
+
+    def __init__(self, delta):
+        self.delta = delta
+
+    def check_type_forward(self, in_types):
+        type_check.expect(in_types.size() == 3)
+        type_check.expect(
+            in_types[0].dtype == numpy.float32,
+            in_types[1].dtype == numpy.float32,
+            in_types[2].dtype == numpy.float32,
+            in_types[0].shape == in_types[1].shape == in_types[2].shape
+        )
+
+    def forward(self, inputs):
+        xp = cuda.get_array_module(*inputs)
+        x, t, tau = inputs
+        self.diff = t - x
+        l1 = xp.abs(self.diff)
+        self.mask = l1 > self.delta
+
+        sign = xp.sign(self.diff)
+        # self.coeff = xp.where(self.diff > 0, tau, 1 - tau)
+        self.coeff = 0.5 + sign * (tau - 0.5)
+
+        l2 = xp.square(self.diff)
+        self.huber = 0.5 * (l2 - self.mask * xp.square(l1 - self.delta))
+        return self.huber * self.coeff,
+
+    def backward(self, inputs, gy):
+        xp = cuda.get_array_module(*inputs)
+
+        sign = xp.sign(self.diff)
+        gd = xp.where(self.mask, sign * self.delta, self.diff)
+        gd *= self.coeff
+        gy_ = gy[0]
+        gd = gy_ * gd
+        gtau = gy_ * sign * self.huber
+        return -gd, gd, gtau
+
+
 def quantile_huber_loss_Dabney(y, t, tau, delta=1.):
     """Quantile Huber loss a la Dabney+
 
-    1710.10044
+    See: https://arxiv.org/abs/1710.10044
     """
-
-    # TODO(kataoka)
-    assert False
+    return QuantileHuberLossDabney(delta)(y, t, tau)
---
+++ tests/functions_tests/loss_tests/test_quantile.py
@@ -0,0 +1,67 @@
+import unittest
+
+import chainer
+from chainer import cuda
+import chainer.functions as F
+from chainer import gradient_check
+from chainer import testing
+from chainer.testing import attr
+import numpy as np
+
+from chainerrl import functions
+# .loss.quantile import quantile_huber_loss_Dabney
+# from chainerrl.functions.loss.quantile import quantile_huber_loss_Dabney
+
+
+@testing.parameterize(
+    {'delta': 0.1},
+    {'delta': 1},
+    {'delta': 10},
+)
+class TestQuantileHuberLossDabney(unittest.TestCase):
+
+    def setUp(self):
+        self.shape = (4, 10)
+        self.x = np.random.standard_normal(self.shape).astype(np.float32)
+        self.t = np.random.standard_normal(self.shape).astype(np.float32)
+        self.tau = np.random.uniform(0, 1, self.shape).astype(np.float32)
+        self.gy = np.random.standard_normal(self.shape).astype(np.float32)
+
+        self.check_backward_options = {'atol': 1e-2, 'rtol': 1e-2}
+                                       # 'no_grads': [False, False, True]}
+                                       # 'no_grads': [True, True, False]}
+
+    def check_forward(self, x_data, t_data, tau_data):
+        x = chainer.Variable(x_data)
+        t = chainer.Variable(t_data)
+        tau = chainer.Variable(tau_data)
+        loss = functions.quantile_huber_loss_Dabney(x, t, tau, self.delta)
+        self.assertEqual(loss.data.dtype, np.float32)
+        loss_value = cuda.to_cpu(loss.data)
+
+        coeff = np.where(t_data - x_data > 0, tau_data, 1 - tau_data)
+        loss_expect = coeff * cuda.to_cpu(
+            F.huber_loss(x, t, self.delta, reduce='no').data)
+        testing.assert_allclose(loss_value, loss_expect)
+
+    def test_forward_cpu(self):
+        self.check_forward(self.x, self.t, self.tau)
+
+    @attr.gpu
+    def test_forward_gpu(self):
+        self.check_forward(cuda.to_gpu(self.x), cuda.to_gpu(self.t),
+                           cuda.to_gpu(self.tau))
+
+    def check_backward(self, x_data, t_data, tau_data, y_grad):
+        gradient_check.check_backward(
+            functions.QuantileHuberLossDabney(self.delta),
+            (x_data, t_data, tau_data), y_grad, eps=1e-2,
+            **self.check_backward_options)
+
+    def test_backward_cpu(self):
+        self.check_backward(self.x, self.t, self.tau, self.gy)
+
+    @attr.gpu
+    def test_backward_gpu(self):
+        self.check_backward(cuda.to_gpu(self.x), cuda.to_gpu(self.t),
+                            cuda.to_gpu(self.tau), cuda.to_gpu(self.gy))
