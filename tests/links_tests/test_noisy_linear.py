2ac1cb55bfbcf3f7df52e6a7256d7d7729f019a0
Toshiki Kataoka <kataoka@preferred.jp>
2018-03-01 19:04:34 +0900

    add gpu tests

--- tests/links_tests/test_noisy_linear.py
+++ tests/links_tests/test_noisy_linear.py
@@ -1,11 +1,20 @@
 import unittest
 
 import chainer
+from chainer import cuda
 from chainer import testing
 from chainer.testing import condition
-import numpy as np
+from chainer.testing import attr
+import numpy
 
 from chainerrl.links import noisy_linear
+
+
+def get_xp_from_id(gpu):
+    if gpu >= 0:
+        return cuda.cupy
+    else:
+        return numpy
 
 
 @testing.parameterize(*testing.product({
@@ -21,37 +30,71 @@ class TestFactorizedNoisyLinear(unittest.TestCase):
         mu = chainer.links.Linear(*self.size_args, nobias=self.nobias)
         self.l = noisy_linear.FactorizedNoisyLinear(mu)
 
-    def test_calls(self):
-        x_data = np.arange(12).astype(np.float32).reshape((2, 6))
+    def _test_calls(self, gpu):
+        xp = get_xp_from_id(gpu)
+        if gpu >= 0:
+            self.l.to_gpu(gpu)
+
+        x_data = xp.arange(12).astype(numpy.float32).reshape((2, 6))
         x = chainer.Variable(x_data)
         self.l(x)
         self.l(x_data + 1)
         self.l(x_data.reshape((2, 3, 2)))
 
-    @condition.retry(3)
-    def test_randomness(self):
-        x = np.random.standard_normal((10, 6)).astype(np.float32)
+    def test_calls_cpu(self):
+        self._test_calls(gpu=-1)
+
+    @attr.gpu
+    def test_calls_gpu(self):
+        self._test_calls(gpu=0)
+
+    def _test_randomness(self, gpu):
+        xp = get_xp_from_id(gpu)
+        if gpu >= 0:
+            self.l.to_gpu(gpu)
+
+        x = xp.random.standard_normal((10, 6)).astype(numpy.float32)
         y1 = self.l(x).data
         y2 = self.l(x).data
-        d = np.mean(np.square(y1 - y2))
+        d = float(xp.mean(xp.square(y1 - y2)))
 
         # The parameter name suggests that
-        # np.sqrt(d / 2) is approx to sigma_scale = 0.4
+        # xp.sqrt(d / 2) is approx to sigma_scale = 0.4
         # In fact, (for each element _[i, j],) it holds:
         # \E[(y2 - y1) ** 2] = 2 * \Var(y) = (4 / pi) * sigma_scale ** 2
 
         target = (0.4 ** 2) * 2
         if self.nobias:
-            target *= 2 / np.pi
+            target *= 2 / numpy.pi
         else:
-            target *= 2 / np.pi + np.sqrt(2 / np.pi)
+            target *= 2 / numpy.pi + numpy.sqrt(2 / numpy.pi)
 
         self.assertGreater(d, target / 3.)
         self.assertLess(d, target * 3.)
 
-    def test_non_randomness(self):
+    @condition.retry(3)
+    def test_randomness_cpu(self):
+        self._test_randomness(gpu=-1)
+
+    @attr.gpu
+    @condition.retry(3)
+    def test_randomness_gpu(self):
+        self._test_randomness(gpu=0)
+
+    def _test_non_randomness(self, gpu):
+        xp = get_xp_from_id(gpu)
+        if gpu >= 0:
+            self.l.to_gpu(gpu)
+
         # Noises should be the same in a batch
-        x0 = np.random.standard_normal((1, 6)).astype(np.float32)
-        x = np.broadcast_to(x0, (2, 6))
+        x0 = xp.random.standard_normal((1, 6)).astype(numpy.float32)
+        x = xp.broadcast_to(x0, (2, 6))
         y = self.l(x).data
-        np.testing.assert_allclose(y[0], y[1], rtol=1e-4)
+        xp.testing.assert_allclose(y[0], y[1], rtol=1e-4)
+
+    def test_non_randomness_cpu(self):
+        self._test_non_randomness(gpu=-1)
+
+    @attr.gpu
+    def test_non_randomness_gpu(self):
+        self._test_non_randomness(gpu=0)
