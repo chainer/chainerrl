a384203f5428123cd89bafe99b71d2799b68f1ab
muupan <muupan@gmail.com>
2018-03-16 19:34:01 +0900

    Add a script to train C51 on ALE

---
+++ examples/ale/train_c51_ale.py
@@ -0,0 +1,134 @@
+from __future__ import print_function
+from __future__ import division
+from __future__ import unicode_literals
+from __future__ import absolute_import
+from builtins import *  # NOQA
+from future import standard_library
+standard_library.install_aliases()
+import argparse
+import os
+
+import chainer
+import numpy as np
+
+import chainerrl
+from chainerrl.envs import ale
+from chainerrl import experiments
+from chainerrl import explorers
+from chainerrl import misc
+from chainerrl import replay_buffer
+
+from dqn_phi import dqn_phi
+
+
+def main():
+    import logging
+    logging.basicConfig(level=logging.DEBUG)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('rom', type=str)
+    parser.add_argument('--outdir', type=str, default='results',
+                        help='Directory path to save output files.'
+                             ' If it does not exist, it will be created.')
+    parser.add_argument('--seed', type=int, default=0,
+                        help='Random seed [0, 2 ** 31)')
+    parser.add_argument('--gpu', type=int, default=0)
+    parser.add_argument('--demo', action='store_true', default=False)
+    parser.add_argument('--load', type=str, default=None)
+    parser.add_argument('--use-sdl', action='store_true', default=False)
+    parser.add_argument('--final-exploration-frames',
+                        type=int, default=10 ** 6)
+    parser.add_argument('--model', type=str, default='')
+    parser.add_argument('--arch', type=str, default='nature',
+                        choices=['nature', 'nips', 'dueling'])
+    parser.add_argument('--steps', type=int, default=10 ** 7)
+    parser.add_argument('--replay-start-size', type=int, default=5 * 10 ** 4)
+    parser.add_argument('--target-update-interval',
+                        type=int, default=10 ** 4)
+    parser.add_argument('--eval-interval', type=int, default=10 ** 5)
+    parser.add_argument('--update-interval', type=int, default=4)
+    parser.add_argument('--activation', type=str, default='relu')
+    parser.add_argument('--eval-n-runs', type=int, default=10)
+    parser.add_argument('--batch-size', type=int, default=32)
+    args = parser.parse_args()
+
+    # Set a random seed used in ChainerRL.
+    misc.set_random_seed(args.seed, gpus=(args.gpu,))
+
+    # Set different random seeds for train and test envs.
+    train_seed = args.seed
+    test_seed = 2 ** 31 - 1 - args.seed
+
+    args.outdir = experiments.prepare_output_dir(args, args.outdir)
+    print('Output files are saved in {}'.format(args.outdir))
+
+    # In training, life loss is considered as terminal states
+    env = ale.ALE(args.rom, use_sdl=args.use_sdl, seed=train_seed)
+    misc.env_modifiers.make_reward_clipped(env, -1, 1)
+    # In testing, an episode is terminated  when all lives are lost
+    eval_env = ale.ALE(args.rom, use_sdl=args.use_sdl,
+                       treat_life_lost_as_terminal=False,
+                       seed=test_seed)
+
+    n_actions = env.number_of_actions
+
+    n_atoms = 51
+    v_max = 10
+    v_min = -10
+    delta_z = (v_max - v_min) / float(n_atoms - 1)
+    z_values = np.array([v_min + i * delta_z
+                         for i in range(n_atoms)], dtype=np.float32)
+    q_func = chainerrl.links.Sequence(
+        chainerrl.links.NatureDQNHead(),
+        chainerrl.q_functions.DistributionalFCStateQFunctionWithDiscreteAction(
+            None, n_actions, n_atoms, z_values,
+            n_hidden_channels=0, n_hidden_layers=0),
+    )
+
+    # Draw the computational graph and save it in the output directory.
+    chainerrl.misc.draw_computational_graph(
+        [q_func(np.zeros((4, 84, 84), dtype=np.float32)[None])],
+        os.path.join(args.outdir, 'model'))
+
+    # Use the same hyper parameters as https://arxiv.org/abs/1707.06887
+    opt = chainer.optimizers.Adam(2.5e-4, eps=1e-2 / args.batch_size)
+    opt.setup(q_func)
+
+    rbuf = replay_buffer.ReplayBuffer(10 ** 6)
+
+    explorer = explorers.LinearDecayEpsilonGreedy(
+        1.0, 0.1,
+        args.final_exploration_frames,
+        lambda: np.random.randint(n_actions))
+    agent = chainerrl.agents.C51(
+        q_func, opt, rbuf, gpu=args.gpu, gamma=0.99,
+        explorer=explorer, replay_start_size=args.replay_start_size,
+        target_update_interval=args.target_update_interval,
+        update_interval=args.update_interval,
+        batch_accumulator='sum', phi=dqn_phi,
+    )
+
+    if args.load:
+        agent.load(args.load)
+
+    if args.demo:
+        eval_stats = experiments.eval_performance(
+            env=eval_env,
+            agent=agent,
+            n_runs=args.eval_n_runs)
+        print('n_runs: {} mean: {} median: {} stdev {}'.format(
+            args.eval_n_runs, eval_stats['mean'], eval_stats['median'],
+            eval_stats['stdev']))
+    else:
+        # In testing DQN, randomly select 5% of actions
+        eval_explorer = explorers.ConstantEpsilonGreedy(
+            5e-2, lambda: np.random.randint(n_actions))
+        experiments.train_agent_with_evaluation(
+            agent=agent, env=env, steps=args.steps,
+            eval_n_runs=args.eval_n_runs, eval_interval=args.eval_interval,
+            outdir=args.outdir, eval_explorer=eval_explorer,
+            eval_env=eval_env)
+
+
+if __name__ == '__main__':
+    main()
