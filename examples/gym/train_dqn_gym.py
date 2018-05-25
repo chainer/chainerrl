6ef5f276426a4b447cd76299b4fd1c90b45e417e
sean <seann999@gmail.com>
2018-05-25 11:56:43 +0900

    rm draw graph

--- examples/gym/train_dqn_gym.py
+++ examples/gym/train_dqn_gym.py
@@ -142,9 +142,9 @@ def main():
         explorer = explorers.Greedy()
 
     # Draw the computational graph and save it in the output directory.
-    chainerrl.misc.draw_computational_graph(
-        [q_func(np.zeros_like(obs_space.low, dtype=np.float32)[None])],
-        os.path.join(args.outdir, 'model'))
+    #chainerrl.misc.draw_computational_graph(
+    #    [q_func(np.zeros_like(obs_space.low, dtype=np.float32)[None])],
+    #    os.path.join(args.outdir, 'model'))
 
     opt = optimizers.Adam()
     opt.setup(q_func)
---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+for num in {1..10}
+do
+dmux run -- python3 examples/gym/train_dqn_gym.py --env MountainCar-v0 --final-exploration-steps 100000 --steps 400000 --reward-scale-factor 1 --seed $num
+dmux run -- python3 examples/gym/train_dqn_gym.py --env MountainCar-v0 --final-exploration-steps 100000 --steps 400000 --reward-scale-factor 1 --noisy-net-sigma 0.5 --seed $num
+dmux run -- python3 examples/gym/train_dqn_gym.py --env MountainCar-v0 --final-exploration-steps 100000 --steps 400000 --reward-scale-factor 1 --noisy-net-sigma 0.5 --noise-constant 0.1 --seed $num
+done
