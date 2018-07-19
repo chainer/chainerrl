f7ebe9de7746dfcf2da35f30d955f519ae3608b6
sean <seann999@gmail.com>
2018-07-19 20:40:21 +0900

    import fix

--- chainerrl/initializers/__init__.py
+++ chainerrl/initializers/__init__.py
@@ -1,3 +1,2 @@
 from chainerrl.initializers.constant import VarianceScalingConstant  # NOQA
 from chainerrl.initializers.normal import LeCunNormal  # NOQA
-from chainerrl.initializers.uniform import VarianceScalingUniform  # NOQA
