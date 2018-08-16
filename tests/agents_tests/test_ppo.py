2fe30f571067ac45769d2e5d83c188c5cec05f50
ljvmiranda921 <ljvmiranda@gmail.com>
2018-08-16 17:35:05 +0900

    Add tests for batch ppo
    This commit adds another test class, TestBatchPPO, to test
    the batch implementation of PPO.
    
    Signed-off-by: ljvmiranda921 <ljvmiranda@gmail.com>

--- tests/agents_tests/test_ppo.py
+++ tests/agents_tests/test_ppo.py
@@ -17,6 +17,8 @@ import numpy as np
 from chainerrl.agents.a3c import A3CSeparateModel
 from chainerrl.agents.ppo import PPO
 from chainerrl.envs.abc import ABC
+from chainerrl.envs.vec_env import VectorEnv
+from chainerrl.experiments import train_agent_batch_with_evaluation
 from chainerrl.experiments import train_agent_with_evaluation
 from chainerrl import policies
 from chainerrl import v_functions
@@ -138,3 +140,129 @@ class TestPPO(unittest.TestCase):
 
     def make_env_and_successful_return(self, test):
         return ABC(discrete=self.discrete, deterministic=test), 1
+
+
+@testing.parameterize(*(
+    testing.product({
+        'clip_eps_vf': [None, 0.2],
+        'lambd': [0.0, 0.5],
+        'discrete': [False, True],
+        'standardize_advantages': [False, True],
+    })
+))
+class TestBatchPPO(unittest.TestCase):
+
+    def setUp(self):
+        self.tmpdir = tempfile.mkdtemp()
+        self.agent_dirname = os.path.join(self.tmpdir, 'agent_final')
+
+    @testing.attr.slow
+    def test_abc_cpu(self):
+        self._test_abc()
+        self._test_abc(steps=0, load_model=True)
+
+    @testing.attr.slow
+    @testing.attr.gpu
+    def test_abc_gpu(self):
+        self._test_abc(gpu=0)
+
+    def test_abc_fast_cpu(self):
+        self._test_abc(steps=100, require_success=False)
+        self._test_abc(steps=0, require_success=False, load_model=True)
+
+    @testing.attr.gpu
+    def test_abc_fast_gpu(self):
+        self._test_abc(steps=100, require_success=False, gpu=0)
+
+    def _test_abc(self, steps=1000000,
+                  require_success=True, gpu=-1, load_model=False, num_envs=3):
+
+        env, _ = self.make_env_and_successful_return(
+            test=False, num_envs=num_envs)
+        test_env, successful_return = self.make_env_and_successful_return(
+            test=True, num_envs=num_envs)
+        agent = self.make_agent(env, gpu)
+
+        if load_model:
+            print('Load agent from', self.agent_dirname)
+            agent.load(self.agent_dirname)
+
+        # Train
+        train_agent_batch_with_evaluation(
+            agent=agent, env=env, steps=steps, outdir=self.tmpdir,
+            eval_interval=200, eval_n_runs=50, successful_score=1,
+            eval_env=test_env)
+
+        agent.stop_episode()
+
+        # Test
+        n_test_runs = 100
+        n_succeeded = np.zeros(num_envs, dtype='f')
+        for _ in range(n_test_runs):
+            total_r = np.zeros(num_envs, dtype='f')
+            obs = test_env.reset()
+            done = np.zeros(num_envs, dtype=bool)
+            reward = np.zeros(num_envs, dtype='f')
+
+            while not done.all():
+                action = agent.batch_act(obs)
+                obs, reward, done, _ = test_env.step(action)
+                total_r += reward
+                success = np.isclose(total_r, successful_return)
+                n_succeeded[success] += 1
+
+        if require_success:
+            self.assertTrue((n_succeeded > 0.8 * n_test_runs).all())
+
+        # Save
+        agent.save(self.agent_dirname)
+        # Close environment
+        env.close()
+        test_env.close()
+
+    def make_agent(self, env, gpu):
+        model = self.make_model(env)
+
+        opt = optimizers.Adam(alpha=3e-4)
+        opt.setup(model)
+
+        return self.make_ppo_agent(env=env, model=model, opt=opt, gpu=gpu)
+
+    def make_ppo_agent(self, env, model, opt, gpu):
+        return PPO(model, opt, gpu=gpu, gamma=0.9, lambd=self.lambd,
+                   update_interval=50, minibatch_size=25, epochs=3,
+                   clip_eps_vf=self.clip_eps_vf,
+                   standardize_advantages=self.standardize_advantages)
+
+    def make_model(self, env):
+        n_hidden_channels = 50
+
+        n_dim_obs = env.observation_space.low.size
+        v = v_functions.FCVFunction(
+            n_dim_obs,
+            n_hidden_layers=2,
+            n_hidden_channels=n_hidden_channels)
+
+        if self.discrete:
+            n_actions = env.action_space.n
+
+            pi = policies.FCSoftmaxPolicy(
+                n_dim_obs, n_actions,
+                n_hidden_layers=2,
+                n_hidden_channels=n_hidden_channels)
+        else:
+            n_dim_actions = env.action_space.low.size
+
+            pi = policies.FCGaussianPolicy(
+                n_dim_obs, n_dim_actions,
+                n_hidden_layers=2,
+                n_hidden_channels=n_hidden_channels)
+
+        return A3CSeparateModel(pi=pi, v=v)
+
+    def make_env_and_successful_return(self, test, num_envs=3):
+        batch_env = VectorEnv([ABC(discrete=self.discrete, deterministic=test)
+                               for _ in range(num_envs)])
+        batch_env.spec = None
+        batch_env.__spec__ = None
+        return batch_env, np.ones(num_envs, dtype='f')
