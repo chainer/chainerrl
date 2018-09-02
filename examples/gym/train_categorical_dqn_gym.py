c90b9bf0b809b31f41a3ebde192f510424327a38
muupan <muupan@gmail.com>
2018-09-02 21:32:52 +0900

    Use CastObservationToFloat32

--- examples/gym/train_categorical_dqn_gym.py
+++ examples/gym/train_categorical_dqn_gym.py
@@ -21,7 +21,6 @@ from chainer import optimizers
 import gym
 gym.undo_logger_setup()  # NOQA
 import gym.wrappers
-import numpy as np
 
 import chainerrl
 from chainerrl import experiments
@@ -80,6 +79,8 @@ def main():
         env = gym.make(args.env)
         env_seed = 2 ** 32 - 1 - args.seed if test else args.seed
         env.seed(env_seed)
+        # Cast observations to float32 because our model uses float32
+        env = chainerrl.wrappers.CastObservationToFloat32(env)
         if args.monitor:
             env = gym.wrappers.Monitor(env, args.outdir)
         if not test:
@@ -135,15 +136,12 @@ def main():
         else:
             rbuf = replay_buffer.ReplayBuffer(rbuf_capacity)
 
-    def phi(obs):
-        return obs.astype(np.float32)
-
     agent = chainerrl.agents.CategoricalDQN(
         q_func, opt, rbuf, gpu=args.gpu, gamma=args.gamma,
         explorer=explorer, replay_start_size=args.replay_start_size,
         target_update_interval=args.target_update_interval,
         update_interval=args.update_interval,
-        phi=phi, minibatch_size=args.minibatch_size,
+        minibatch_size=args.minibatch_size,
         target_update_method=args.target_update_method,
         soft_update_tau=args.soft_update_tau,
         episodic_update=args.episodic_replay, episodic_update_len=16)
