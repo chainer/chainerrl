af931bed8f28ab58270c655817fbe3f4dfb64ced
muupan <muupan@gmail.com>
2018-09-16 00:33:31 +0900

    Add RandomizeAction to replace eval_explorer

--- chainerrl/wrappers/__init__.py
+++ chainerrl/wrappers/__init__.py
@@ -1,2 +1,4 @@
 from chainerrl.wrappers.cast_observation import CastObservation  # NOQA
 from chainerrl.wrappers.cast_observation import CastObservationToFloat32  # NOQA
+
+from chainerrl.wrappers.randomize_action import RandomizeAction  # NOQA
