3b60688c50ced04b89268d25bd3ff13bdce00b10
muupan <muupan@gmail.com>
2018-10-11 19:29:51 +0900

    Test batch training tests for the whole DQN family.
    Adam's eps is tuned so that tests finish faster.

--- tests/agents_tests/test_double_dqn.py
+++ tests/agents_tests/test_double_dqn.py
@@ -6,20 +6,14 @@ from builtins import *  # NOQA
 from future import standard_library
 standard_library.install_aliases()  # NOQA
 
+import basetest_dqn_like
+from basetest_training import _TestBatchTrainingMixin
 from chainerrl.agents.double_dqn import DoubleDQN
 
-import basetest_dqn_like
 
-
-class TestDoubleDQNOnDiscreteABC(basetest_dqn_like._TestDQNOnDiscreteABC):
-
-    def make_dqn_agent(self, env, q_func, opt, explorer, rbuf, gpu):
-        return DoubleDQN(
-            q_func, opt, rbuf, gpu=gpu, gamma=0.9, explorer=explorer,
-            replay_start_size=100, target_update_interval=100)
-
-
-class TestDoubleDQNOnContinuousABC(basetest_dqn_like._TestDQNOnContinuousABC):
+class TestDoubleDQNOnDiscreteABC(
+        _TestBatchTrainingMixin,
+        basetest_dqn_like._TestDQNOnDiscreteABC):
 
     def make_dqn_agent(self, env, q_func, opt, explorer, rbuf, gpu):
         return DoubleDQN(
@@ -27,6 +21,17 @@ class TestDoubleDQNOnContinuousABC(basetest_dqn_like._TestDQNOnContinuousABC):
             replay_start_size=100, target_update_interval=100)
 
 
+class TestDoubleDQNOnContinuousABC(
+        _TestBatchTrainingMixin,
+        basetest_dqn_like._TestDQNOnContinuousABC):
+
+    def make_dqn_agent(self, env, q_func, opt, explorer, rbuf, gpu):
+        return DoubleDQN(
+            q_func, opt, rbuf, gpu=gpu, gamma=0.9, explorer=explorer,
+            replay_start_size=100, target_update_interval=100)
+
+
+# Batch training with recurrent models is currently not supported
 class TestDoubleDQNOnDiscretePOABC(basetest_dqn_like._TestDQNOnDiscretePOABC):
 
     def make_dqn_agent(self, env, q_func, opt, explorer, rbuf, gpu):
