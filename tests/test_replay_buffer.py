6d379ff6843b139c66b4c8cb241a5370945878e7
Prabhat Nagarajan <prabhat.nagarajan@gmail.com>
2018-10-24 01:20:49 -0500

    modifies all replay_buffer tests to match new specs, everything passes

--- tests/test_replay_buffer.py
+++ tests/test_replay_buffer.py
@@ -263,18 +263,18 @@ class TestPrioritizedReplayBuffer(unittest.TestCase):
             self.assertEqual(s2[1], correct_item2)
 
         # Weights should be different for different TD-errors
-        # s3 = rbuf.sample(2)
-        # self.assertNotAlmostEqual(s3[0]['weight'], s3[1]['weight'])
+        s3 = rbuf.sample(2)
+        self.assertNotAlmostEqual(s3[0][0]['weight'], s3[1][0]['weight'])
 
         # # Weights should be equal for different but clipped TD-errors
-        # rbuf.update_errors([5, 10])
-        # s3 = rbuf.sample(2)
-        # self.assertAlmostEqual(s3[0]['weight'], s3[1]['weight'])
+        rbuf.update_errors([5, 10])
+        s3 = rbuf.sample(2)
+        self.assertAlmostEqual(s3[0][0]['weight'], s3[1][0]['weight'])
 
         # # Weights should be equal for the same TD-errors
-        # rbuf.update_errors([3.14, 3.14])
-        # s4 = rbuf.sample(2)
-        # self.assertAlmostEqual(s4[0]['weight'], s4[1]['weight'])
+        rbuf.update_errors([3.14, 3.14])
+        s4 = rbuf.sample(2)
+        self.assertAlmostEqual(s4[0][0]['weight'], s4[1][0]['weight'])
 
     def test_capacity(self):
         capacity = self.capacity
@@ -298,17 +298,23 @@ class TestPrioritizedReplayBuffer(unittest.TestCase):
 
     def test_save_and_load(self):
         capacity = self.capacity
+        num_steps = self.num_steps
 
         tempdir = tempfile.mkdtemp()
 
-        rbuf = replay_buffer.PrioritizedReplayBuffer(capacity)
+        rbuf = replay_buffer.PrioritizedReplayBuffer(capacity, num_steps=num_steps)
 
         # Add two transitions
-        trans1 = dict(state=0, action=1, reward=2, next_state=3,
-                      next_action=4, is_state_terminal=True)
-        rbuf.append(**trans1)
+        correct_item = collections.deque([], maxlen=num_steps)
+        for _ in range(num_steps):
+            trans1 = dict(state=0, action=1, reward=2, next_state=3,
+                          next_action=4, is_state_terminal=False)
+            correct_item.append(trans1)
+            rbuf.append(**trans1)
+        correct_item2 = copy.deepcopy(correct_item)
         trans2 = dict(state=1, action=1, reward=2, next_state=3,
                       next_action=4, is_state_terminal=True)
+        correct_item2.append(trans2)
         rbuf.append(**trans2)
 
         # Now it has two transitions
@@ -319,7 +325,7 @@ class TestPrioritizedReplayBuffer(unittest.TestCase):
         rbuf.save(filename)
 
         # Initialize rbuf
-        rbuf = replay_buffer.PrioritizedReplayBuffer(capacity)
+        rbuf = replay_buffer.PrioritizedReplayBuffer(capacity, num_steps=num_steps)
 
         # Of course it has no transition yet
         self.assertEqual(len(rbuf), 0)
@@ -332,14 +338,14 @@ class TestPrioritizedReplayBuffer(unittest.TestCase):
 
         # And sampled transitions are exactly what I added!
         s2 = rbuf.sample(2)
-        del s2[0]['weight']
-        del s2[1]['weight']
-        if s2[0]['state'] == 0:
-            self.assertEqual(s2[0], trans1)
-            self.assertEqual(s2[1], trans2)
+        del s2[0][0]['weight']
+        del s2[1][0]['weight']
+        if s2[0][num_steps-1]['state'] == 0:
+            self.assertEqual(s2[0], correct_item)
+            self.assertEqual(s2[1], correct_item2)
         else:
-            self.assertEqual(s2[0], trans2)
-            self.assertEqual(s2[1], trans1)
+            self.assertEqual(s2[0], correct_item2)
+            self.assertEqual(s2[1], correct_item)
 
 
 def exp_return_of_episode(episode):
