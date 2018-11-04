9495a43e0797d1a089df644663900957cadc3ac0
muupan <muupan@gmail.com>
2018-11-04 17:24:22 +0900

    Test multiple values of N and N_prime

--- tests/agents_tests/test_iqn.py
+++ tests/agents_tests/test_iqn.py
@@ -8,12 +8,17 @@ standard_library.install_aliases()  # NOQA
 
 import chainer.functions as F
 import chainer.links as L
+from chainer import testing
 
 import basetest_dqn_like as base
 import chainerrl
 from chainerrl.agents import iqn
 
 
+@testing.parameterize(*testing.product({
+    'quantile_thresholds_N': [1, 5],
+    'quantile_thresholds_N_prime': [1, 7],
+}))
 class TestIQNOnDiscreteABC(base._TestDQNOnDiscreteABC):
 
     def make_q_func(self, env):
@@ -31,4 +36,7 @@ class TestIQNOnDiscreteABC(base._TestDQNOnDiscreteABC):
     def make_dqn_agent(self, env, q_func, opt, explorer, rbuf, gpu):
         return iqn.IQN(
             q_func, opt, rbuf, gpu=gpu, gamma=0.9, explorer=explorer,
-            replay_start_size=100, target_update_interval=100)
+            replay_start_size=100, target_update_interval=100,
+            quantile_thresholds_N=self.quantile_thresholds_N,
+            quantile_thresholds_N_prime=self.quantile_thresholds_N_prime,
+        )
