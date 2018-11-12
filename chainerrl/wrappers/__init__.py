7bf437459690b2c937926b34c0712f6bd5713877
Prabhat Nagarajan <prabhat.nagarajan@gmail.com>
2018-11-12 00:53:04 -0600

    removes extra lines from __init__

--- chainerrl/wrappers/__init__.py
+++ chainerrl/wrappers/__init__.py
@@ -4,4 +4,3 @@ from chainerrl.wrappers.cast_observation import CastObservationToFloat32  # NOQA
 from chainerrl.wrappers.randomize_action import RandomizeAction  # NOQA
 
 from chainerrl.wrappers.scale_reward import ScaleReward  # NOQA
-
