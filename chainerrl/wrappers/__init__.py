d5af43d9c248a55712a2438f7130ddb5f481f779
Prabhat Nagarajan <prabhat.nagarajan@gmail.com>
2018-11-12 00:52:02 -0600

    removes the atari specific envs from the wrapper init

--- chainerrl/wrappers/__init__.py
+++ chainerrl/wrappers/__init__.py
@@ -5,12 +5,3 @@ from chainerrl.wrappers.randomize_action import RandomizeAction  # NOQA
 
 from chainerrl.wrappers.scale_reward import ScaleReward  # NOQA
 
-from chainerrl.wrappers.atari_wrappers import LazyFrames  # NOQA
-from chainerrl.wrappers.atari_wrappers import ScaledFloatFrame  # NOQA
-from chainerrl.wrappers.atari_wrappers import FrameStack  # NOQA
-from chainerrl.wrappers.atari_wrappers import WarpFrame  # NOQA
-from chainerrl.wrappers.atari_wrappers import ClipRewardEnv  # NOQA
-from chainerrl.wrappers.atari_wrappers import MaxAndSkipEnv  # NOQA
-from chainerrl.wrappers.atari_wrappers import EpisodicLifeEnv  # NOQA
-from chainerrl.wrappers.atari_wrappers import FireResetEnv  # NOQA
-from chainerrl.wrappers.atari_wrappers import NoopResetEnv  # NOQA
