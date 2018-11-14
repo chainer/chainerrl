7e874cc789b4bd81f4a6d594bc1a404ab9073191
Yasuhiro Fujita <muupan@gmail.com>
2018-11-14 16:34:54 +0900

    Update to v0.5.0

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ if sys.version_info < (3, 5):
     install_requires.append('funcsigs')
 
 setup(name='chainerrl',
-      version='0.4.0',
+      version='0.5.0',
       description='ChainerRL, a deep reinforcement learning library',
       author='Yasuhiro Fujita',
       author_email='fujita@preferred.jp',
