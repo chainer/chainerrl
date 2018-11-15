690ce637ebe4c02ccacaa2b4251dc8e3232eec5d
muupan <muupan@gmail.com>
2018-11-15 19:36:34 +0900

    Fix import of atari_wrappers

--- examples/ale/train_iqn_ale.py
+++ examples/ale/train_iqn_ale.py
@@ -20,8 +20,7 @@ from chainerrl import experiments
 from chainerrl import explorers
 from chainerrl import misc
 from chainerrl import replay_buffer
-
-import atari_wrappers
+from chainerrl.wrappers import atari_wrappers
 
 
 def main():
