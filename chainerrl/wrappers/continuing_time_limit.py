bac52cd60f233386e0153b23be1ba1399c87106e
muupan <muupan@gmail.com>
2018-11-16 17:21:08 +0900

    Clean ContinuingTimeLimit

--- chainerrl/wrappers/continuing_time_limit.py
+++ chainerrl/wrappers/continuing_time_limit.py
@@ -1,54 +1,48 @@
-import logging
-import time
+from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+from builtins import *  # NOQA
+from future import standard_library
+standard_library.install_aliases()  # NOQA
 
 import gym
 
 
-logger = logging.getLogger(__name__)
-
-
 class ContinuingTimeLimit(gym.Wrapper):
-    def __init__(self, env, max_episode_seconds=None, max_episode_steps=None):
+    """TimeLimit wrapper for continuing environments.
+
+    This is similar gym.wrappers.TimeLimit, which sets a time limit for
+    each episode, except that done=False is returned and that
+    info['needs_reset'] is set to True when past the limit.
+
+    Code that calls env.step is repsonsible for checking the info dict, the
+    fourth returned value, and resetting the env if it has the 'needs_reset'
+    key and its value is True.
+
+    Args:
+        env (gym.Env): Env to wrap.
+        max_episode_steps (int): Maximum number of timesteps during an episode,
+            after which the env needs a reset.
+    """
+
+    def __init__(self, env, max_episode_steps):
         super(ContinuingTimeLimit, self).__init__(env)
-        self._max_episode_seconds = max_episode_seconds
         self._max_episode_steps = max_episode_steps
 
-        self._elapsed_steps = 0
-        self._episode_started_at = None
+        self._elapsed_steps = None
 
-    @property
-    def _elapsed_seconds(self):
-        return time.time() - self._episode_started_at
-
-    def _past_limit(self):
-        """Return true if we are past our limit"""
-        if (self._max_episode_steps is not None
-                and self._max_episode_steps <= self._elapsed_steps):
-            logger.debug("Env has passed the step limit defined by TimeLimit.")
-            return True
-
-        if (self._max_episode_seconds is not None
-                and self._max_episode_seconds <= self._elapsed_seconds):
-            logger.debug(
-                "Env has passed the seconds limit defined by TimeLimit.")
-            return True
-
-        return False
-
-    def _step(self, action):
-        assert self._episode_started_at is not None,\
+    def step(self, action):
+        assert self._elapsed_steps is not None,\
             "Cannot call env.step() before calling reset()"
         observation, reward, done, info = self.env.step(action)
         self._elapsed_steps += 1
 
-        if self._past_limit():
-            if self.metadata.get('semantics.autoreset'):
-                self.reset()  # automatically reset the env
+        if self._max_episode_steps <= self._elapsed_steps:
             info['needs_reset'] = True
 
         return observation, reward, done, info
 
-    def _reset(self):
-        self._episode_started_at = time.time()
+    def reset(self):
         self._elapsed_steps = 0
         return self.env.reset()
