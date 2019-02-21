26ae4e35cca2edaa8349c8ba9227cd90107303b0
Prabhat Nagarajan <prabhat.nagarajan@gmail.com>
2019-02-21 06:05:47 -0600

    sets eval interval to match paper

--- examples/gym/train_her_gym.py
+++ examples/gym/train_her_gym.py
@@ -94,7 +94,7 @@ def main():
     parser.add_argument('--soft-update-tau', type=float, default=1e-2)
     parser.add_argument('--update-interval', type=int, default=16 * 50)
     parser.add_argument('--eval-n-runs', type=int, default=100)
-    parser.add_argument('--eval-interval', type=int, default=10 ** 5)
+    parser.add_argument('--eval-interval', type=int, default=50 * 16 * 50)
     parser.add_argument('--gamma', type=float, default=0.98)
     parser.add_argument('--minibatch-size', type=int, default=128)
     parser.add_argument('--render', action='store_true')
