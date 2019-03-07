1b714cf33fec25271096e79b8ebe9bcfad0fe9de
Prabhat Nagarajan <prabhat.nagarajan@gmail.com>
2019-03-07 04:46:34 -0600

    adds rainbow to test examples, and cleans up the example more to align more with paper

--- examples/atari/rainbow/train_rainbow.py
+++ examples/atari/rainbow/train_rainbow.py
@@ -25,25 +25,7 @@ from chainerrl.q_functions \
 from chainerrl import replay_buffer
 
 from chainerrl.wrappers import atari_wrappers
-
-
-def parse_arch(arch, n_actions, n_atoms, v_min, v_max):
-    if arch == 'plain':
-        return chainerrl.links.Sequence(
-            chainerrl.links.NatureDQNHead(),
-            DistributionalFCStateQFunctionWithDiscreteAction(
-                None, n_actions, n_atoms, v_min, v_max,
-                n_hidden_channels=0, n_hidden_layers=0),
-        )
-    elif arch == 'dueling':
-        return DistributionalDuelingDQN(n_actions, n_atoms, v_min, v_max,)
-    else:
-        raise RuntimeError('Not supported architecture: {}'.format(arch))
-
-
-def parse_agent(agent):
-    return {'CDQN': agents.CategoricalDQN,
-            'DoubleCDQN': agents.CategoricalDoubleDQN}[agent]
+import json
 
 
 def main():
@@ -58,27 +40,20 @@ def main():
     parser.add_argument('--demo', action='store_true', default=False)
     parser.add_argument('--load', type=str, default=None)
     parser.add_argument('--use-sdl', action='store_true', default=False)
-    parser.add_argument('--final-exploration-frames',
-                        type=int, default=10 ** 6)
     parser.add_argument('--final-epsilon', type=float, default=0.1)
     parser.add_argument('--eval-epsilon', type=float, default=0.05)
-    parser.add_argument('--noisy-net-sigma', type=float, default=None)
-    parser.add_argument('--arch', type=str, default='plain',
-                        choices=['plain', 'dueling'],
-                        help='Network architecture to use.')
-    parser.add_argument('--steps', type=int, default=10 ** 7)
+    parser.add_argument('--noisy-net-sigma', type=float, default=0.5)
+    parser.add_argument('--steps', type=int, default=5 * 10 ** 7)
     parser.add_argument('--max-frames', type=int,
                         default=30 * 60 * 60,  # 30 minutes with 60 fps
                         help='Maximum number of frames for each episode.')
-    parser.add_argument('--replay-start-size', type=int, default=5 * 10 ** 4)
+    parser.add_argument('--replay-start-size', type=int, default=8 * 10 ** 4)
     parser.add_argument('--target-update-interval',
                         type=int, default=3.2 * 10 ** 4)
-    parser.add_argument('--eval-interval', type=int, default=10 ** 5)
+    parser.add_argument('--eval-n-steps', type=int, default=125000)
+    parser.add_argument('--eval-interval', type=int, default=250000)
     parser.add_argument('--update-interval', type=int, default=4)
-    parser.add_argument('--eval-n-runs', type=int, default=10)
-    parser.add_argument('--num-step-return', type=int, default=1)
-    parser.add_argument('--agent', type=str, default='CDQN',
-                        choices=['CDQN', 'DoubleCDQN'])
+    parser.add_argument('--num-step-return', type=int, default=3)
     parser.add_argument('--batch-size', type=int, default=32)
     parser.add_argument('--logging-level', type=int, default=20,
                         help='Logging level. 10:DEBUG, 20:INFO etc.')
@@ -89,6 +64,7 @@ def main():
                              ' are saved as output files.')
     parser.add_argument('--prioritized', action='store_true', default=False,
                         help='Use prioritized experience replay.')
+    parser.add_argument('--n-best-episodes', type=int, default=200)    
     args = parser.parse_args()
 
     import logging
@@ -131,17 +107,12 @@ def main():
     n_atoms = 51
     v_max = 10
     v_min = -10
-    q_func = parse_arch(args.arch, n_actions, n_atoms, v_min, v_max)
-
-    if args.noisy_net_sigma is not None:
-        links.to_factorized_noisy(q_func)
-        # Turn off explorer
-        explorer = explorers.Greedy()
-    else:
-        explorer = explorers.LinearDecayEpsilonGreedy(
-            1.0, args.final_epsilon,
-            args.final_exploration_frames,
-            lambda: np.random.randint(n_actions))
+    q_func = DistributionalDuelingDQN(n_actions, n_atoms, v_min, v_max,)
+    
+    # Noisy nets
+    links.to_factorized_noisy(q_func)
+    # Turn off explorer
+    explorer = explorers.Greedy()
 
     # Draw the computational graph and save it in the output directory.
     chainerrl.misc.draw_computational_graph(
@@ -166,7 +137,7 @@ def main():
         # Feature extractor
         return np.asarray(x, dtype=np.float32) / 255
 
-    Agent = parse_agent(args.agent)
+    Agent = agents.CategoricalDoubleDQN
     agent = Agent(
         q_func, opt, rbuf, gpu=args.gpu, gamma=0.99,
         explorer=explorer, minibatch_size=args.batch_size,
@@ -184,20 +155,44 @@ def main():
         eval_stats = experiments.eval_performance(
             env=eval_env,
             agent=agent,
-            n_steps=None,
-            n_episodes=args.eval_n_runs)
-        print('n_runs: {} mean: {} median: {} stdev {}'.format(
-            args.eval_n_runs, eval_stats['mean'], eval_stats['median'],
+            n_steps=args.eval_n_steps,
+            n_episodes=None)
+        print('n_episodes: {} mean: {} median: {} stdev {}'.format(
+            eval_stats['episodes'],
+            eval_stats['mean'],
+            eval_stats['median'],
             eval_stats['stdev']))
+
     else:
         experiments.train_agent_with_evaluation(
             agent=agent, env=env, steps=args.steps,
-            eval_n_steps=None,
-            eval_n_episodes=args.eval_n_runs, eval_interval=args.eval_interval,
+            eval_n_steps=args.eval_n_steps,
+            eval_n_episodes=None,
+            eval_interval=args.eval_interval,
             outdir=args.outdir,
-            save_best_so_far_agent=False,
+            save_best_so_far_agent=True,
             eval_env=eval_env,
         )
+
+        dir_of_best_network = os.path.join(args.outdir, "best")
+        agent.load(dir_of_best_network)
+
+        # run 30 evaluation episodes, each capped at 5 mins of play
+        stats = experiments.evaluator.eval_performance(
+            env=eval_env,
+            agent=agent,
+            n_steps=None,
+            n_episodes=args.n_best_episodes,
+            max_episode_len=27000,
+            logger=None)
+        with open(os.path.join(args.outdir, 'bestscores.json'), 'w') as f:
+            # temporary hack to handle python 2/3 support issues.
+            # json dumps does not support non-string literal dict keys
+            json_stats = json.dumps(stats)
+            print(str(json_stats), file=f)
+        print("The results of the best scoring network:")
+        for stat in stats:
+            print(str(stat) + ":" + str(stats[stat]))
 
 
 if __name__ == '__main__':
