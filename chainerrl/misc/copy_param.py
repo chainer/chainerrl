cd7d11fe7f7e4086e1cf4357490b65c3ff6b36b7
muupan <muupan@gmail.com>
2019-03-10 18:18:26 +0900

    Use [...] instead of [:] to support scalar arrays

--- chainerrl/misc/copy_param.py
+++ chainerrl/misc/copy_param.py
@@ -19,15 +19,15 @@ def copy_param(target_link, source_link):
                 'not initialized.\nPlease try to forward dummy input '
                 'beforehand to determine parameter shape of the model.'.format(
                     param_name))
-        target_params[param_name].array[:] = param.array
+        target_params[param_name].array[...] = param.array
 
     # Copy Batch Normalization's statistics
     target_links = dict(target_link.namedlinks())
     for link_name, link in source_link.namedlinks():
         if isinstance(link, L.BatchNormalization):
             target_bn = target_links[link_name]
-            target_bn.avg_mean[:] = link.avg_mean
-            target_bn.avg_var[:] = link.avg_var
+            target_bn.avg_mean[...] = link.avg_mean
+            target_bn.avg_var[...] = link.avg_var
 
 
 def soft_copy_param(target_link, source_link, tau):
@@ -40,25 +40,25 @@ def soft_copy_param(target_link, source_link, tau):
                 'not initialized.\nPlease try to forward dummy input '
                 'beforehand to determine parameter shape of the model.'.format(
                     param_name))
-        target_params[param_name].array[:] *= (1 - tau)
-        target_params[param_name].array[:] += tau * param.array
+        target_params[param_name].array[...] *= (1 - tau)
+        target_params[param_name].array[...] += tau * param.array
 
     # Soft-copy Batch Normalization's statistics
     target_links = dict(target_link.namedlinks())
     for link_name, link in source_link.namedlinks():
         if isinstance(link, L.BatchNormalization):
             target_bn = target_links[link_name]
-            target_bn.avg_mean[:] *= (1 - tau)
-            target_bn.avg_mean[:] += tau * link.avg_mean
-            target_bn.avg_var[:] *= (1 - tau)
-            target_bn.avg_var[:] += tau * link.avg_var
+            target_bn.avg_mean[...] *= (1 - tau)
+            target_bn.avg_mean[...] += tau * link.avg_mean
+            target_bn.avg_var[...] *= (1 - tau)
+            target_bn.avg_var[...] += tau * link.avg_var
 
 
 def copy_grad(target_link, source_link):
     """Copy gradients of a link to another link."""
     target_params = dict(target_link.namedparams())
     for param_name, param in source_link.namedparams():
-        target_params[param_name].grad[:] = param.grad
+        target_params[param_name].grad[...] = param.grad
 
 
 def synchronize_parameters(src, dst, method, tau=None):
