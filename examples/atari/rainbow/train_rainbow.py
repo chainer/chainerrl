ae78f1aa1459a97a5db98e3f61b07283a5a5acf8
Prabhat Nagarajan <prabhat.nagarajan@gmail.com>
2019-03-30 09:27:58 -0500

    removes unnecessary cprof stuff

--- examples/atari/rainbow/train_rainbow.py
+++ examples/atari/rainbow/train_rainbow.py
@@ -162,8 +162,6 @@ def main():
             eval_stats['stdev']))
 
     else:
-        pr = cProfile.Profile()
-        pr.enable()
         experiments.train_agent_with_evaluation(
             agent=agent, env=env, steps=args.steps,
             eval_n_steps=args.eval_n_steps,
@@ -173,18 +171,6 @@ def main():
             save_best_so_far_agent=True,
             eval_env=eval_env,
         )
-
-        pr.disable()
-        # s = io.StringIO()
-        # ps.print_stats()
-        # thing = pr.print_stats()
-        with open(os.path.join(args.outdir, 'rainbow.prof'), 'w') as f:
-            ps = pstats.Stats(pr, stream=f).sort_stats('tottime')
-            ps.print_stats()
-            # sys.stdout = f
-            # # f.write(s.getvalue())
-            # pr.print_stats(f)
-            # sys.stdout = sys.__stdout__
 
         dir_of_best_network = os.path.join(args.outdir, "best")
         agent.load(dir_of_best_network)
