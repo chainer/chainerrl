2a1fb7916db76b33a6302f8bc645e53a13d481c3
Prabhat Nagarajan <prabhat.nagarajan@gmail.com>
2019-04-04 07:50:32 -0500

    adds some print statements

--- chainerrl/links/noisy_linear.py
+++ chainerrl/links/noisy_linear.py
@@ -40,6 +40,8 @@ class FactorizedNoisyLinear(chainer.Chain):
 
     def _eps(self, shape, dtype):
         xp = self.xp
+        print("The Matrix lib is " + str(self.xp))
+        print("The datatype is " + str(dtype))
         r = xp.random.standard_normal(shape).astype(dtype)
 
         # apply the function f
