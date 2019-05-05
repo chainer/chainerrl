9b867904c192ef48998c8428276e7a0078bbcb06
muupan <muupan@gmail.com>
2019-05-05 17:01:51 +0900

    Add mujoco/td3 to test_examples.sh

--- test_examples.sh
+++ test_examples.sh
@@ -142,3 +142,8 @@ python examples/grasping/train_dqn_batch_grasping.py --demo --load $model --eval
 python examples/mujoco/ddpg/train_ddpg.py --env Pendulum-v0 --gpu $gpu --steps 10 --replay-start-size 5 --batch-size 5 --outdir $outdir/mujoco/ddpg
 model=$(find $outdir/mujoco/ddpg -name "*_finish")
 python examples/mujoco/ddpg/train_ddpg.py --env Pendulum-v0 --demo --load $model --eval-n-runs 1 --outdir $outdir/temp --gpu $gpu
+
+# mujoco/td3 (specify non-mujoco env to test without mujoco)
+python examples/mujoco/td3/train_td3.py --env Pendulum-v0 --gpu $gpu --steps 10 --replay-start-size 5 --batch-size 5 --outdir $outdir/mujoco/td3
+model=$(find $outdir/mujoco/td3 -name "*_finish")
+python examples/mujoco/td3/train_td3.py --env Pendulum-v0 --demo --load $model --eval-n-runs 1 --outdir $outdir/temp --gpu $gpu
