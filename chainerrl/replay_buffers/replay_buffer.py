c2ce3b7a9b990e39bb750b565340854fffdaae29
Prabhat Nagarajan <prabhat.nagarajan@gmail.com>
2019-06-14 05:31:52 -0500

    ports buffers

--- chainerrl/replay_buffers/replay_buffer.py
+++ chainerrl/replay_buffers/replay_buffer.py
@@ -1,4 +1,13 @@
+from __future__ import print_function
+from __future__ import unicode_literals
+from __future__ import division
+from __future__ import absolute_import
+from builtins import *  # NOQA
+from future import standard_library
+standard_library.install_aliases()
+
 from chainerrl import replay_buffer
+
 
 class ReplayBuffer(replay_buffer.AbstractReplayBuffer):
 
