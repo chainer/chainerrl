5fef490a592cd4e2595d33d4ac0e3bd8c0b0d1fd
Toshiki Kataoka <kataoka@preferred.jp>
2019-06-26 18:39:16 +0900

    Add tests with Sequence and Sequential

--- tests/links_tests/test_noisy_chain.py
+++ tests/links_tests/test_noisy_chain.py
@@ -1,7 +1,9 @@
 import unittest
 
 import chainer
+import chainer.testing
 
+import chainerrl
 from chainerrl.links import to_factorized_noisy
 
 
@@ -43,3 +45,44 @@ class TestToFactorizedNoisy(unittest.TestCase):
             {
                 '/l1', '/l1/mu', '/l1/sigma',
                 '/l2', '/l2/mu', '/l2/sigma', '/l3'})
+
+    def test_sequence(self):
+        ch = chainerrl.links.Sequence(
+            chainer.links.Linear(3),
+            chainer.functions.relu,
+            chainer.links.Linear(4),
+        )
+        self.assertEqual(
+            names_of_links(ch),
+            {'/0', '/1'}
+        )
+        self.assertIs(ch.layers[1], chainer.functions.relu)
+        to_factorized_noisy(ch)
+        self.assertEqual(
+            names_of_links(ch),
+            {
+                '/0', '/0/mu', '/0/sigma',
+                '/1', '/1/mu', '/1/sigma',
+            })
+        self.assertIs(ch.layers[1], chainer.functions.relu)
+
+    @chainer.testing.with_requires('chainer>=5')
+    def test_sequential(self):
+        ch = chainer.Sequential(
+            chainer.links.Linear(3),
+            chainer.functions.relu,
+            chainer.links.Linear(4),
+        )
+        self.assertEqual(
+            names_of_links(ch),
+            {'/0', '/1'}
+        )
+        self.assertIs(ch._layers[1], chainer.functions.relu)
+        to_factorized_noisy(ch)
+        self.assertEqual(
+            names_of_links(ch),
+            {
+                '/0', '/0/mu', '/0/sigma',
+                '/1', '/1/mu', '/1/sigma',
+            })
+        self.assertIs(ch._layers[1], chainer.functions.relu)
