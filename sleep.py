44b8e725df9c34f87e8b4a9fb468afaad2b30f53
Avinash Ummadisingu <u.avinash4x@gmail.com>
2019-06-27 09:58:57 +0900

    Remove sleep.py

--- sleep.py
+++
@@ -1,8 +0,0 @@
-import time
-
-
-if __name__=="__main__":
-    print("Script started!!!")
-    for i in range(200):
-        print("Sleeping for",(200-i)*60," seconds more!")
-        time.sleep(60)
