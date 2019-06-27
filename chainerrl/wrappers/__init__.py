a2345f837dfb1684836dbabbc155b4b23ff56896
NAKATA Keisuke <keisuke.nakata.919@gmail.com>
2019-06-27 13:40:55 +0900

    Monitor with ContinuingTimeLimit support

--- chainerrl/wrappers/__init__.py
+++ chainerrl/wrappers/__init__.py
@@ -1,7 +1,7 @@
 from chainerrl.wrappers.cast_observation import CastObservation  # NOQA
 from chainerrl.wrappers.cast_observation import CastObservationToFloat32  # NOQA
 
-from chainerrl.wrappers.continuing_time_limit import ContinuingTimeLimit  # NOQA
+from chainerrl.wrappers.continuing_time_limit import ContinuingTimeLimit, ContinuingTimeLimitMonitor  # NOQA
 
 from chainerrl.wrappers.randomize_action import RandomizeAction  # NOQA
 
