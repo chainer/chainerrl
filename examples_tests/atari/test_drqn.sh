cf848d6a47d4bdb4f5f023729d54e0a23c518da1
muupan <muupan@gmail.com>
2019-07-01 20:37:32 +0900

    Remove examples_tests/ale/test_drqn.sh

--- examples_tests/atari/test_drqn.sh
+++ examples_tests/atari/test_drqn.sh
@@ -6,7 +6,7 @@ outdir=$(mktemp -d)
 
 gpu="$1"
 
-# atari/drqn
-python examples/atari/train_drqn_ale.py --env AsterixNoFrameskip-v4 --flicker --recurrent --steps 100 --replay-start-size 50 --outdir $outdir/atari/reproduction/dqn --eval-n-steps 200 --eval-interval 50 --gpu $gpu
-model=$(find $outdir/atari/reproduction/dqn -name "*_finish")
-python examples/atari/train_drqn_ale.py --env AsterixNoFrameskip-v4 --flicker --recurrent --demo --load $model --outdir $outdir/temp --eval-n-steps 200 --demo-n-episodes 1 --gpu $gpu
+# ale/drqn
+python examples/atari/train_drqn_ale.py --env PongNoFrameskip-v4 --steps 100 --replay-start-size 50 --outdir $outdir/ale/dqn --gpu $gpu --recurrent --flicker
+model=$(find $outdir/ale/dqn -name "*_finish")
+python examples/atari/train_drqn_ale.py --env PongNoFrameskip-v4 --demo --load $model --demo-n-episodes 1 --max-frames 50 --outdir $outdir/temp --gpu $gpu --recurrent --flicker
