255dc45ac8aad36ae363ceba5f99c25ba9923865
Prabhat Nagarajan <prabhat.nagarajan@gmail.com>
2019-07-01 02:24:21 -0500

    skips v4 in tests for examples

--- examples_tests/atari/test_collect_demos.sh
+++ examples_tests/atari/test_collect_demos.sh
@@ -6,7 +6,12 @@ outdir=$(mktemp -d)
 
 gpu="$1"
 
-# ale/collect_demos
-python examples/ale/train_dqn_ale.py --env PongNoFrameskip-v4 --steps 100 --replay-start-size 50 --outdir $outdir/ale/dqn --gpu $gpu
-model=$(find $outdir/ale/dqn -name "*_finish")
-python examples/ale/collect_demos_ale.py --env PongNoFrameskip-v4 --load $model --steps 100 --outdir $outdir/temp --gpu $gpu
+# Chainer 4 does not support open_pickle_dataset_writer, which is used by the demonstration collection example.
+pickle_writer_support=$(python -c "import chainer; from distutils.version import StrictVersion; print(1 if StrictVersion(chainer.__version__) >= StrictVersion('3.1.0') else 0)")
+
+# atari/collect_demos
+if [[ $pickle_writer_support = 1 ]]; then
+	python examples/atari/train_dqn_ale.py --env PongNoFrameskip-v4 --steps 100 --replay-start-size 50 --outdir $outdir/atari/dqn --gpu $gpu
+	model=$(find $outdir/atari/dqn -name "*_finish")
+	python examples/atari/collect_demos_ale.py --env PongNoFrameskip-v4 --load $model --steps 100 --outdir $outdir/temp --gpu $gpu
+fi
