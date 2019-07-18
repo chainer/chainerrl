e67a9bb736d6ee9f74a7b17cb8b75730bf963418
muupan <muupan@gmail.com>
2019-07-18 16:25:16 +0900

    Fix paths

--- examples_tests/mujoco/reproduction/test_soft_actor_critic.sh
+++ examples_tests/mujoco/reproduction/test_soft_actor_critic.sh
@@ -7,6 +7,6 @@ outdir=$(mktemp -d)
 gpu="$1"
 
 # mujoco/soft_actor_critic (specify non-mujoco env to test without mujoco)
-python examples/mujoco/soft_actor_critic/train_soft_actor_critic.py --env Pendulum-v0 --gpu $gpu --steps 10 --replay-start-size 5 --batch-size 5 --outdir $outdir/mujoco/soft_actor_critic
+python examples/mujoco/reproduction/soft_actor_critic/train_soft_actor_critic.py --env Pendulum-v0 --gpu $gpu --steps 10 --replay-start-size 5 --batch-size 5 --outdir $outdir/mujoco/soft_actor_critic
 model=$(find $outdir/mujoco/soft_actor_critic -name "*_finish")
-python examples/mujoco/soft_actor_critic/train_soft_actor_critic.py --env Pendulum-v0 --demo --load $model --eval-n-runs 1 --outdir $outdir/temp --gpu $gpu
+python examples/mujoco/reproduction/soft_actor_critic/train_soft_actor_critic.py --env Pendulum-v0 --demo --load $model --eval-n-runs 1 --outdir $outdir/temp --gpu $gpu
