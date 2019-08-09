22d13df41f8ee3aee08bb8bf20218df5037df92e
Yasuhiro Fujita <muupan@gmail.com>
2019-08-09 17:18:17 +0900

    Update examples_tests/atari/test_drqn.sh
    Co-Authored-By: Prabhat Nagarajan <prabhat.nagarajan@gmail.com>

--- examples_tests/atari/test_drqn.sh
+++ examples_tests/atari/test_drqn.sh
@@ -8,5 +8,5 @@ gpu="$1"
 
 # atari/drqn
 python examples/atari/train_drqn_ale.py --env PongNoFrameskip-v4 --steps 100 --replay-start-size 50 --outdir $outdir/atari/drqn --gpu $gpu --recurrent --flicker
-model=$(find $outdir/atari/dqn -name "*_finish")
+model=$(find $outdir/atari/drqn -name "*_finish")
 python examples/atari/train_drqn_ale.py --env PongNoFrameskip-v4 --demo --load $model --demo-n-episodes 1 --max-frames 50 --outdir $outdir/temp --gpu $gpu --recurrent --flicker
