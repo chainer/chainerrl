0157f1496f833f75eacfa5a388c74cec1486756b
muupan <muupan@gmail.com>
2019-08-25 04:28:52 +0900

    Check ubuntu version to support both 18.04 and 16.04

--- .pfnci/run.sh
+++ .pfnci/run.sh
@@ -54,11 +54,14 @@ main() {
       --hint="/chainerrl/.pfnci/hint.pbtxt"
   )
 
-  # Because ffmpeg of official repository causes segmentation fault,
-  # we use jonathonf/ffmpeg-3
-  apt-get update -q
-  apt-get install -qy --no-install-recommends software-properties-common
-  add-apt-repository ppa:jonathonf/ffmpeg-3
+  UBUNTU_VERSION_ID=$(grep DISTRIB_RELEASE /etc/lsb-release | cut -d "=" -f2)
+  if [ "$UBUNTU_VERSION_ID" = "16.04" ]; then
+    # Because ffmpeg of ubuntu 16.04 causes segmentation fault,
+    # we use jonathonf/ffmpeg-3
+    apt-get update -q
+    apt-get install -qy --no-install-recommends software-properties-common
+    add-apt-repository ppa:jonathonf/ffmpeg-3
+  fi
 
   apt-get update -q
   apt-get install -qy --no-install-recommends \
