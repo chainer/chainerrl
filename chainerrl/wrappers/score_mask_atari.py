aa690ee8848879ed0d27ff13c33a7375802084d4
Prabhat <prabhat.nagarajan@gmail.com>
2019-09-02 22:20:03 +0900

    adds some additional functionality and cleanup

--- chainerrl/wrappers/score_mask_atari.py
+++ chainerrl/wrappers/score_mask_atari.py
@@ -13,33 +13,48 @@ def make_atari(env_id, max_frames=30 * 60 * 60, mask_render=False):
     if max_frames:
         env = chainerrl.wrappers.ContinuingTimeLimit(
             env, max_episode_steps=max_frames)
-    if env_id == "SpaceInvadersNoFrameskip-v4":
-        env = SpaceInvadersScoreMask(env, mask_render)
-    else:
-        assert False, "Not a supported env"
+    env = ScoreMaskEnv(env, mask_render)
     env = atari_wrappers.NoopResetEnv(env, noop_max=30)
     env = atari_wrappers.MaxAndSkipEnv(env, skip=4)
     return env
 
 
-class SpaceInvadersScoreMask(gym.Wrapper):
+class AtariMask():
+    def __init__(self, env):
+        if "SpaceInvadersNoFrameskip" in env.spec.id:
+            self.mask = self.mask_space_invaders
+        else:
+            assert False, "Not a supported env"
+            self.mask = lambda x: x
+
+    def __call__(self, x):
+        return self.mask(x)
+
+    def mask_space_invaders(self, obs):
+        mask_obs = obs
+        # mask out score
+        # TODO: check whether spaceship comes
+        mask_obs[0:20] = 0
+        return mask_obs
+
+class ScoreMaskEnv(gym.Wrapper):
     def __init__(self, env, mask_render):
         """ Masked env
         """
-        assert 'SpaceInvadersNoFrameskip' in env.spec.id
         gym.Wrapper.__init__(self, env)
         self.obs = None
         self.mask_render = mask_render
+        self.mask = AtariMask(env)
 
     def reset(self, **kwargs):
         obs =  self.env.reset(**kwargs)
-        mask_obs = self.mask_obs(obs)
+        mask_obs = self.mask(obs)
         self.obs = mask_obs
         return mask_obs
 
     def step(self, ac):
         obs, reward, done, info = self.env.step(ac)
-        mask_obs = self.mask_obs(obs)
+        mask_obs = self.mask(obs)
         self.obs = mask_obs
         return mask_obs, reward, done, info
 
@@ -52,10 +67,3 @@ class SpaceInvadersScoreMask(gym.Wrapper):
             return self.viewer.isopen
         else:
             return self.env.render(mode)
-
-    def mask_obs(self, obs):
-        mask_obs = obs
-        # mask out score
-        # TODO: check whether spaceship comes
-        mask_obs[0:20] = 0
-        return mask_obs
--- chainerrl/wrappers/trex_reward.py
+++ chainerrl/wrappers/trex_reward.py
@@ -7,12 +7,21 @@ from future import standard_library
 standard_library.install_aliases()  # NOQA
 
 import chainer
+from chainer import optimizers
 import chainer.functions as F
 import chainer.links as L
 import gym
 import numpy as np
 
 from chainerrl.misc.batch_states import batch_states
+
+
+def random_subseq(seq, subseq_len, start=None):
+    if len(seq) <= subseq_len:
+        return seq
+    else:
+        i = np.random.randint(0, len(seq) - subseq_len + 1)
+        return seq[i:i + subseq_len]
 
 
 class TREXNet(chainer.ChainList):
@@ -44,9 +53,11 @@ class TREXReward(gym.Wrapper):
     https://arxiv.org/abs/1904.06387.
 
     Args:
-        env: Env to wrap.
-        demos (RankedDemoDataset): A list of ranked demonstrations
-        network: A reward network
+        env: a ScoreMaskEnv
+        ranked_demos (RankedDemoDataset): A list of ranked demonstrations
+        steps: number of gradient steps
+        sub_traj_len: a tuple containing (min, max) traj length to sample
+        network: A reward network to train
 
     Attributes:
         demos: A list of demonstrations
@@ -54,11 +65,20 @@ class TREXReward(gym.Wrapper):
 
     """
 
-    def __init__(self, env, ranked_demos, network=TREXNet()):
+    def __init__(self, env,
+                 ranked_demos,
+                 steps=30000,
+                 sub_traj_len=(50,100),
+                 optimizer=optimizers.Adam(),
+                 network=TREXNet()):
         super().__init__(env)
         self.ranked_demos = ranked_demos
+        self.steps = steps
         self.trex_network = network
+        self.opt = optimizer
+        self.opt.setup(self.trex_network)
         self.prev_reward = None
+        self.mask = env.mask
         self._train()
 
     def step(self, action):
@@ -66,14 +86,17 @@ class TREXReward(gym.Wrapper):
         obs = batch_states([observation], self.trex_network.xp, self._phi)
         with chainer.no_backprop_mode():
             trex_reward = self.trex_network(obs)
-        return observation, reward, done, info
+        return observation, trex_reward, done, info
 
     def _train(self):
+
+        for _ in range(self.steps):
+            pass
+        # use the mask
         #TODOs: sample and align by time
         # subsample 6000 trajectory pairs between 50 and 100 observations long. 
         # We optimized the reward functions using Adam with a learning rate of 5e-5 for 30,000 steps.
         # enduro exception 
-        pass
 
     def _phi(self, x):
         return np.asarray(x, dtype=np.float32) / 255
