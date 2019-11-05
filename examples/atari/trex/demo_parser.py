52185ea70a106ab22699b398153005abd09bfcf2
Prabhat Nagarajan <prabhat@Prabhats-MacBook-Pro.local>
2019-11-05 19:27:11 +0900

    fixes axis

--- examples/atari/trex/demo_parser.py
+++ examples/atari/trex/demo_parser.py
@@ -231,7 +231,7 @@ class AtariGrandChallengeParser():
                 stacked_frames.append(np.expand_dims(new_ep_screens[0], 0))
             for m in range(len(new_ep_screens)):
                 tmp_new_screens.append(atari_wrappers.LazyFrames(list(stacked_frames),
-                                       stack_axis=stack_axis))
+                                       stack_axis=0))
                 stacked_frames.append(np.expand_dims(new_ep_screens[m], 0))
             new_ep_screens = tmp_new_screens
 
