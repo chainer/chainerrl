7fb72124a59de26afcb230e5d6b4c0b62999d2e3
muupan <muupan@gmail.com>
2020-02-12 20:18:16 +0900

    Avoid using assert_called_once sine it's from py3.6

--- tests/experiments_tests/test_train_agent_async.py
+++ tests/experiments_tests/test_train_agent_async.py
@@ -88,7 +88,7 @@ class TestTrainAgentAsync(unittest.TestCase):
 
         # All the envs (including eval envs) should to be closed
         for env in envs + eval_envs:
-            env.close.assert_called_once()
+            assert env.close.call_count == 1
 
         if self.num_envs == 1:
             self.assertEqual(hook.call_count, steps)
